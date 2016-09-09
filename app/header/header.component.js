af2de403609e27e4866d97a93a3775c3e7faeff4
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-09 13:29:06 +0200

    adjust fullscreen dialog breakpoint to be xs, not xs + sm

--- app/header/header.component.js
+++ app/header/header.component.js
@@ -7,17 +7,17 @@ angular.module('header').component('header', {
     function HeaderController($mdDialog) {
       var header = this;
   
-      header.showLogin = function(evt) {
+      header.showLoginDialog = function(evt) {
         $mdDialog.show({
-          controller: LoginController,
-          controllerAs: 'login',
-          templateUrl: 'header/login.template.html',
+          controller: LoginDialogController,
+          controllerAs: 'loginDialog',
+          templateUrl: 'header/loginDialog.template.html',
           parent: angular.element(document.body),
           targetEvent: evt,
           openFrom: angular.element(document.body),
           closeTo: angular.element(document.body),
           clickOutsideToClose: true,
-          fullscreen: header.customFullscreen // Only for -xs, -sm breakpoints.
+          fullscreen: true // Only for -xs, -sm breakpoints.
         })
         .then(function(answer) {
           //
@@ -26,7 +26,7 @@ angular.module('header').component('header', {
         });
       };
 
-      function LoginController($mdDialog) {
+      function LoginDialogController($mdDialog) {
         var login = this;
         login.hide = function() {
           $mdDialog.hide();
