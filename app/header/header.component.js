761a5cec1d1473219ca194b11ed40f91e25d9137
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-09 14:26:58 +0200

    add sign up dialog and add toolbar to dialogs

--- app/header/header.component.js
+++ app/header/header.component.js
@@ -7,13 +7,13 @@ angular.module('header').component('header', {
     function HeaderController($mdDialog) {
       var header = this;
   
-      header.showLoginDialog = function(evt) {
+      header.showLoginDialog = function(event) {
         $mdDialog.show({
           controller: LoginDialogController,
           controllerAs: 'loginDialog',
           templateUrl: 'header/loginDialog.template.html',
           parent: angular.element(document.body),
-          targetEvent: evt,
+          targetEvent: event,
           openFrom: angular.element(document.body),
           closeTo: angular.element(document.body),
           clickOutsideToClose: true,
@@ -29,6 +29,32 @@ angular.module('header').component('header', {
       function LoginDialogController($mdDialog) {
         var login = this;
         login.hide = function() {
+          $mdDialog.hide();
+        }
+      }
+
+      header.showSignupDialog = function(event) {
+        $mdDialog.show({
+          controller: SignupDialogController,
+          controllerAs: 'signupDialog',
+          templateUrl: 'header/signupDialog.template.html',
+          parent: angular.element(document.body),
+          targetEvent: event,
+          openFrom: angular.element(document.body),
+          closeTo: angular.element(document.body),
+          clickOutsideToClose: true,
+          fullscreen: true // Only for -xs, -sm breakpoints.
+        })
+        .then(function(answer) {
+          //
+        }, function() {
+          //
+        });
+      }
+
+      function SignupDialogController($mdDialog) {
+        var signup = this;
+        signup.hide = function() {
           $mdDialog.hide();
         }
       }
