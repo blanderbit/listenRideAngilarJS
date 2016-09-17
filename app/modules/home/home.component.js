10566e4c457750e72ea05a5275fd9253d90e5bf9
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-17 22:09:11 +0200

    adds search bar in home page

--- app/modules/home/home.component.js
+++ app/modules/home/home.component.js
@@ -3,12 +3,18 @@
 angular.module('home').component('home', {
   templateUrl: 'modules/home/home.template.html',
   controllerAs: 'home',
-  controller: [ 'api', 
-    function HomeController(api) {
+  controller: [ 'api', '$state',
+    function HomeController(api, $state) {
       var home = this;
+
       api.get("/featured").then(function(response) {
         home.featuredBikes = response.data;
       });
+
+      home.searchQuery;
+      home.onSearch = function() {
+        $state.go('search', {location: home.searchQuery});
+      };
     }
   ]
 });
