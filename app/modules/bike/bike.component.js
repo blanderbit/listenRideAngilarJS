c4f65523ad6241bc25b2736c58b059f56ca0e9fd
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-20 01:46:08 +0200

    add bike photo dialog

--- app/modules/bike/bike.component.js
+++ app/modules/bike/bike.component.js
@@ -3,8 +3,8 @@
 angular.module('bike').component('bike', {
   templateUrl: 'modules/bike/bike.template.html',
   controllerAs: 'bike',
-  controller: ['api', '$stateParams', 'uiGmapGoogleMapApi',
-    function BikeController(api, $stateParams, uiGmapGoogleMapApi) {
+  controller: ['api', '$stateParams', '$mdDialog', 'uiGmapGoogleMapApi',
+    function BikeController(api, $stateParams, $mdDialog, uiGmapGoogleMapApi) {
       var bike = this;
 
       bike.map = {
@@ -16,11 +16,6 @@ angular.module('bike').component('bike', {
           },
           markers: []
       };
-      
-
-      uiGmapGoogleMapApi.then(function(maps) {
-          // console.log(maps);
-      });
 
       api.get('/rides/' + $stateParams.bikeId).then(function success(response) {
         console.log("Successfully retrieved Bike");
@@ -35,7 +30,41 @@ angular.module('bike').component('bike', {
         
       }, function error() {
         console.log("Error retrieving User");
-      })
+      });
+
+      bike.showGalleryDialog = function(event) {
+        $mdDialog.show({
+          controller: GalleryDialogController,
+          controllerAs: 'galleryDialog',
+          templateUrl: 'modules/bike/galleryDialog.template.html',
+          locals: {
+            bikeData: bike.data
+          },
+          parent: angular.element(document.body),
+          targetEvent: event,
+          openFrom: angular.element(document.body),
+          closeTo: angular.element(document.body),
+          clickOutsideToClose: true,
+          fullscreen: true // Changed in CSS to only be for XS sizes
+        })
+        .then(function(answer) {
+          //
+        }, function() {
+          //
+        });
+      };
+
+      function GalleryDialogController($mdDialog, bikeData) {
+        var galleryDialog = this;
+        galleryDialog.image_1 = bikeData.image_file_1.image_file_1.url;
+        galleryDialog.image_2 = bikeData.image_file_2.image_file_2.url;
+        galleryDialog.image_3 = bikeData.image_file_3.image_file_3.url;
+        galleryDialog.image_4 = bikeData.image_file_4.image_file_4.url;
+        galleryDialog.image_5 = bikeData.image_file_5.image_file_5.url;
+        galleryDialog.hide = function() {
+          $mdDialog.hide();
+        }
+      }
     }
   ]
 });
