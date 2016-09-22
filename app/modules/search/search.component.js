ae5a160d0d911d9fb1ebe2406f55b166e087c04a
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-22 15:09:17 +0200

    adds infowindows

--- app/modules/search/search.component.js
+++ app/modules/search/search.component.js
@@ -9,15 +9,32 @@ angular.module('search').component('search', {
   controller: ['$http', 'NgMap', 'api',
     function SearchController($http, NgMap, api) {
       var search = this;
+      search.mapOptions = {
+        lat: 40,
+        lng: -74,
+        zoom: 10
+      };
 
       NgMap.getMap().then(function(map) {
-        console.log(map.getCenter());
-        console.log('markers', map.markers);
-        console.log('shapes', map.shapes);
+        search.map = map;
       });
+
+      search.showBikeWindow = function(evt, bikeId) {
+        for (let i = 0; i < search.bikes.length; ++i) {
+          if (search.bikes[i].id == bikeId) {
+            search.selectedBike = search.bikes[i];
+            break;
+          }
+        }
+        search.map.showInfoWindow('mapWindow', this);
+      };
 
       api.get("/rides?location=" + search.location).then(function(response) {
         search.bikes = response.data;
+        if (search.bikes.length > 0) {
+          search.mapOptions.lat = search.bikes[0].lat_rnd;
+          search.mapOptions.lng = search.bikes[0].lng_rnd;
+        }
       });
 
     }
