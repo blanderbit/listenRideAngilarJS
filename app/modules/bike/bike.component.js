6c4eb7d9eaffd74eeb89b9710a637906e266e487
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-22 18:11:47 +0200

    adds ng-map with circle to profile page

--- app/modules/bike/bike.component.js
+++ app/modules/bike/bike.component.js
@@ -3,31 +3,23 @@
 angular.module('bike').component('bike', {
   templateUrl: 'modules/bike/bike.template.html',
   controllerAs: 'bike',
-  controller: ['api', '$stateParams', '$mdDialog', 'uiGmapGoogleMapApi',
-    function BikeController(api, $stateParams, $mdDialog, uiGmapGoogleMapApi) {
+  controller: ['api', '$stateParams', '$mdDialog', 'NgMap',
+    function BikeController(api, $stateParams, $mdDialog, NgMap) {
       var bike = this;
 
-      bike.map = {
-          // center: { latitude: bike.data.lat_rnd, longitude: bike.data.lng_rnd },
-          center: { latitude: 0, longitude: 0 },
-          zoom: 12,
-          options: {
-            scrollwheel: false
-          },
-          markers: []
+      bike.mapOptions = {
+          lat: 0,
+          lng: 0,
+          zoom: 14,
+          radius: 500
       };
 
-      api.get('/rides/' + $stateParams.bikeId).then(function success(response) {
-        console.log("Successfully retrieved Bike");
+      api.get('/rides/' + $stateParams.bikeId)
+      .then(function success(response) {
         bike.data = response.data;
-        console.log(bike);
-        console.log(bike.data.image_file_1.image_file_1.large.url);
 
-        bike.map.center = {
-          latitude: bike.data.lat_rnd,
-          longitude: bike.data.lng_rnd
-        }
-        
+        bike.mapOptions.lat = bike.data.lat_rnd;
+        bike.mapOptions.lng = bike.data.lng_rnd;
       }, function error() {
         console.log("Error retrieving User");
       });
