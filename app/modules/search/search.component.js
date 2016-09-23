f0df25df9caa3eecc1f361e4f3bb086a165e98ae
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-23 15:47:51 +0200

    adds filter by height

--- app/modules/search/search.component.js
+++ app/modules/search/search.component.js
@@ -9,6 +9,26 @@ angular.module('search').component('search', {
   controller: ['$http', 'NgMap', 'api',
     function SearchController($http, NgMap, api) {
       var search = this;
+
+      search.filterOptions = {
+        size: "",
+        allterrain: true,
+        city: true,
+        electric: true,
+        kids: true,
+        race: true,
+        special: true
+      }
+
+      search.sizeOptions = [
+        {value: "", label: "all"},
+        {value: 155, label: "155 - 165"},
+        {value: 165, label: "165 - 175"},
+        {value: 175, label: "175 - 185"},
+        {value: 185, label: "185 - 195"},
+        {value: 195, label: "195 - 205"}
+      ];
+
       search.mapOptions = {
         lat: 40,
         lng: -74,
@@ -28,6 +48,8 @@ angular.module('search').component('search', {
 
       api.get("/rides?location=" + search.location).then(function(response) {
         search.bikes = response.data;
+        search.bikes.map( function(bike) {console.log(bike.size)} );
+
         if (search.bikes.length > 0) {
           search.mapOptions.lat = search.bikes[0].lat_rnd;
           search.mapOptions.lng = search.bikes[0].lng_rnd;
