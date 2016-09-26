34ff80460b88c93f6c61e0c1d1a4ddbcaef9c8cf
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-26 18:24:03 +0200

    add message component

--- app/modules/_shared/message/message.component.js
+++ app/modules/_shared/message/message.component.js
@@ -11,9 +11,18 @@ angular.module('message').component('message', {
     timestamp: '<',
     request: '<'
   },
-  controller: [ '$translate',
-    function MessageController($translate) {
+  controller: [ '$translate', '$localStorage',
+    function MessageController($translate, $localStorage) {
       var message = this;
+
+      message.chatFlow = function() {
+        if ($localStorage.userId == message.request.user.id) {
+          return "rideChat";
+        }
+        else {
+          return "listChat";
+        }
+      };
 
       message.sentMessage = function() {
         return message.status == null && message.request.user.id == message.receiver;
@@ -24,22 +33,7 @@ angular.module('message').component('message', {
       }
 
       message.statusMessage = function() {
-        return message.status != null;
-      }
-
-      message.statusContent = function(statusId) {
-        switch(statusId) {
-          case 1: return $translate.instant('listChat.would_like_rent_your', {riderName: message.request.user.first_name + " " + message.request.user.last_name, riderCity: message.request.user.city, bikeName: message.request.ride.name, timespan: message.request.timespan,  duration: message.request.duration});
-          case 2: return $translate.instant('listChat.you_accepted_request');
-          case 3: return $translate.instant('listChat.rider_confirmed_booking', {riderName: message.request.user.first_name + " " + message.request.user.last_name, riderCity: message.request.user.city, riderZip: message.request.user.zip, riderStreet: message.request.user.street});
-          case 4:
-          case 5:
-          case 6:
-          case 7:
-          case 8:
-          case 9:
-          default: 
-        }
+        return message.status != null && message.status != 6 && message.status != 7;
       }
     }
   ]
