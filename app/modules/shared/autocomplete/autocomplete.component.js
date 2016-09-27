d24bc4bd9e0acacab6616e92d9e79dbd1aaa7757
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-27 18:02:48 +0200

    adds persistant filters

--- app/modules/shared/autocomplete/autocomplete.component.js
+++ app/modules/shared/autocomplete/autocomplete.component.js
@@ -4,17 +4,21 @@ angular.module('autocomplete').component('autocomplete', {
   templateUrl: 'app/modules/shared/autocomplete/autocomplete.template.html',
   controllerAs: 'autocomplete',
   bindings: {
-    value: '<'
+    location: '='
   },
-  controller: ['$state',
-    function AutocompleteController($state) {
+  controller: [
+    function AutocompleteController() {
       var autocomplete = this;
 
-      autocomplete.autocomplete = new google.maps.places.Autocomplete(document.getElementById('searchBox'));
-
-      autocomplete.onSearch = function() {
-        $state.go('search', {location: document.getElementById('searchBox').value});
-      };
+      autocomplete.autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocompleteSearch'));
+      autocomplete.autocomplete.addListener('place_changed', function() {
+        autocomplete.location = autocomplete.autocomplete.getPlace().formatted_address;
+        /*      
+          console.log("getPlace autocomplete.value", autocomplete.location);
+          console.log("getPlace lat", autocomplete.autocomplete.getPlace().geometry.location.lat());
+          console.log("getPlace lng", autocomplete.autocomplete.getPlace().geometry.location.lng());
+        */
+      });
     }
   ]
 });
