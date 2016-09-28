c93e9c206aa1b600ab65fcba147efbfcd64119ce
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-28 15:03:27 +0200

    adds base structure for the calendar component

---
+++ app/modules/bike/calendar.component.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('bike').component('calendar', {
+  templateUrl: 'app/modules/bike/calendar.template.html',
+  controllerAs: 'calendar',
+  controller: [
+    function CalendarController() {
+      var calendar = this;
+      
+    }
+  ]
+});
