37aaa7d1237e2b319ff6eeb0a4a8c7ddb1c40816
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-09-29 12:40:13 +0200

    fix responsive request view

--- app/modules/requests/requests.component.js
+++ app/modules/requests/requests.component.js
@@ -3,8 +3,8 @@
 angular.module('requests').component('requests', {
   templateUrl: 'app/modules/requests/requests.template.html',
   controllerAs: 'requests',
-  controller: ['$localStorage', '$interval', '$mdMedia', 'api',
-    function RequestsController($localStorage, $interval, $mdMedia, api) {
+  controller: ['$localStorage', '$interval', '$mdMedia', '$mdDialog', 'api',
+    function RequestsController($localStorage, $interval, $mdMedia, $mdDialog, api) {
       var requests = this;
 
       var poller;
@@ -26,21 +26,44 @@ angular.module('requests').component('requests', {
           if (requests.request.messages == null || (requests.request.messages.length != success.data.messages.length)) {
             requests.request = success.data;
             requests.request.rideChat = $localStorage.userId == requests.request.user.id;
-            console.log(requests.request);
           }
         }, function(error) {
           console.log("Error fetching request");
         });
       };
 
+      var showChatDialog = function() {
+        $mdDialog.show({
+          controller: ChatDialogController,
+          controllerAs: 'chatDialog',
+          templateUrl: 'app/modules/requests/chatDialog.template.html',
+          parent: angular.element(document.body),
+          targetEvent: event,
+          openFrom: angular.element(document.body),
+          closeTo: angular.element(document.body),
+          clickOutsideToClose: false,
+          fullscreen: true // Only for -xs, -sm breakpoints.
+        })
+        .then(function(answer) {
+          //
+        }, function() {
+          //
+        });
+      }
+
       requests.loadRequest = function(requestId) {
-        requests.showChat = true;
         $interval.cancel(poller);
         reloadRequest(requestId);
         poller = $interval(function() {
-            console.log("polling");
             reloadRequest(requestId);
         }, 10000);
+
+        if ($mdMedia('xs')) {
+          requests.showChat = false;
+          showChatDialog();
+        } else {
+          requests.showChat = true;
+        }
       }
 
       requests.$onDestroy = function() {
@@ -59,12 +82,21 @@ angular.module('requests').component('requests', {
         };
         requests.request.messages.push(data);
         api.post('/messages', message).then(function(success) {
-          requests.loadRequest(requests.request.id);
+          //
         }, function(error) {
           console.log("Error occured sending message");
         });
 
         requests.message = "";
+      }
+
+      var ChatDialogController = function() {
+        var chatDialog = this;
+        chatDialog.requests = requests;
+
+        chatDialog.hide = function() {
+          $mdDialog.hide();
+        };
       }
 
     }
