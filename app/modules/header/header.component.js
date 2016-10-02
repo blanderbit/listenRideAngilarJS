e0b80e26ef094b4bd34686aa2b8a441349aa92b5
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-10-03 01:49:55 +0200

    add dropdown to logged in header

--- app/modules/header/header.component.js
+++ app/modules/header/header.component.js
@@ -3,11 +3,23 @@
 angular.module('header').component('header', {
   templateUrl: 'app/modules/header/header.template.html',
   controllerAs: 'header',
-  controller: ['$mdSidenav', 'authentication', 'verification',
-    function HeaderController($mdSidenav, authentication, verification) {
+  controller: ['$mdSidenav', '$localStorage', 'api', 'authentication', 'verification',
+    function HeaderController($mdSidenav, $localStorage, api, authentication, verification) {
       var header = this;
       header.authentication = authentication;
       header.verification = verification;
+
+      if (authentication.loggedIn) {
+        api.get('/users/' + $localStorage.userId).then(
+          function (success) {
+            console.log(success.data.profile_picture.profile_picture.url);
+            header.profilePictureUrl = success.data.profile_picture.profile_picture.url;
+          },
+          function (error) {
+
+          }
+        );
+      }
 
       header.toggleSidebar = function() {
         $mdSidenav('right').toggle();
