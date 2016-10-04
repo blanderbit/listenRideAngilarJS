466ea9b5e18efe1740c633a3cd94ee90d29b50e9
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-10-04 13:09:00 +0200

    allow search onEnter

--- app/modules/shared/autocomplete/autocomplete.component.js
+++ app/modules/shared/autocomplete/autocomplete.component.js
@@ -12,8 +12,8 @@ angular.module('autocomplete').component('autocomplete', {
 
       autocomplete.autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocompleteSearch'));
       autocomplete.autocomplete.addListener('place_changed', function() {
-        autocomplete.location = autocomplete.autocomplete.getPlace().formatted_address;
-        /*      
+        /*
+          autocomplete.location = autocomplete.autocomplete.getPlace().formatted_address;
           console.log("getPlace autocomplete.value", autocomplete.location);
           console.log("getPlace lat", autocomplete.autocomplete.getPlace().geometry.location.lat());
           console.log("getPlace lng", autocomplete.autocomplete.getPlace().geometry.location.lng());
