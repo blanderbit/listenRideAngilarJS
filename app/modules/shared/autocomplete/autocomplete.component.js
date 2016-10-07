a89394efbbd3154760bad5c644895a3cd8b845c3
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-10-07 19:18:34 +0200

    solves autocomplete scroll bug

--- app/modules/shared/autocomplete/autocomplete.component.js
+++ app/modules/shared/autocomplete/autocomplete.component.js
@@ -6,19 +6,33 @@ angular.module('autocomplete').component('autocomplete', {
   bindings: {
     location: '='
   },
-  controller: [
-    function AutocompleteController() {
+  controller: ['$interval',
+    function AutocompleteController($interval) {
       var autocomplete = this;
 
-      autocomplete.autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocompleteSearch'));
-      autocomplete.autocomplete.addListener('place_changed', function() {
-        /*
-          autocomplete.location = autocomplete.autocomplete.getPlace().formatted_address;
-          console.log("getPlace autocomplete.value", autocomplete.location);
-          console.log("getPlace lat", autocomplete.autocomplete.getPlace().geometry.location.lat());
-          console.log("getPlace lng", autocomplete.autocomplete.getPlace().geometry.location.lng());
-        */
-      });
+      initAutocomplete();
+
+      function initAutocomplete() {
+        var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocompleteSearch'));
+        autocomplete.addListener('place_changed', function() {
+          
+        });
+
+        var timer = $interval(function() {
+          if ($(".pac-container").length > 0) {
+            var el = $(".pac-container").detach();
+            el.appendTo("autocomplete");
+            $interval.cancel(timer);
+          }
+        }, 100);
+      }
+      
+      /*
+        autocomplete.location = autocomplete.autocomplete.getPlace().formatted_address;
+        console.log("getPlace autocomplete.value", autocomplete.location);
+        console.log("getPlace lat", autocomplete.autocomplete.getPlace().geometry.location.lat());
+        console.log("getPlace lng", autocomplete.autocomplete.getPlace().geometry.location.lng());
+      */
     }
   ]
 });
