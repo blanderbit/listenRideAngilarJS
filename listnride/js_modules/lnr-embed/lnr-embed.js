cbf971d648cf7d90d942b7d456ed7d0a498824b2
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-12-08 22:45:38 +0100

    updates in bike rendering. proper api call. styling applied correctly.

--- listnride/js_modules/lnr-embed/lnr-embed.js
+++ listnride/js_modules/lnr-embed/lnr-embed.js
@@ -5,7 +5,7 @@
   scr_lnr.type = 'text/javascript';
 
   var css_lnr = document.createElement("LINK");
-  css_lnr.href = "https://listnride-frontend.herokuapp.com/lnr-embed.min.css";
+  css_lnr.href = "/Users/aurangzaib/Documents/Projects/listnride/listnride-frontend/listnride/js_modules/lnr-embed/lnr-embed.css";
   css_lnr.rel = "stylesheet";
 
   var css_mdl = document.createElement("LINK");
@@ -22,31 +22,59 @@
   head.appendChild(css_lnr);
   head.appendChild(scr_lnr);
 
-  var checkReady = function (callback) {
+  var renderBikes = function (callback) {
     if (window.jQuery) {
       callback(jQuery);
     } else {
       window.setTimeout(function () {
-        checkReady(callback);
+        renderBikes(callback);
       }, 100);
     }
   };
 
-  // Start polling...
-  checkReady(function ($) {
+  renderBikes(function ($) {
     $(function () {
-      $.get("https://api.listnride.com/v2/featured", function (a) {
-        var id_lnr = $("#listnride");
-        id_lnr.append('<div class="mdl-grid" id="lnr-grid"></div>');
-        var grid = $("#lnr-grid");
-        a.forEach(function (a) {
+      var id_lnr = $("#listnride");
+      var user_id = document.getElementById('listnride').dataset.user;
+      id_lnr.append('<div class="mdl-grid" id="lnr-grid"></div>');
+      var grid = $("#lnr-grid");
+      console.log("user id: ", user_id);
+      $.get("https://listnride-staging.herokuapp.com/v2/users/" + user_id, function (response) {
+        response.rides.forEach(function (ride) {
+          
+          var imageUrl = ride.image_file_1.image_file_1.small.url;
+          var brand = ride.brand;
+          var category = ride.category;
+          var price = ride.price_daily;
+          
           grid.append(
-            '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
-            a.image_file_1.image_file_1.small.url +
-            '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand +
-            ", " + a.category + "</div></div></div>");
+            '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle">' +
+            '<bike-card class="flex-gt-xs-50 flex-gt-sm-33 flex-100">' +
+            '<md-card class="lnr-bike-card _md" href="https://www.google.com">' +
+            '<img class="md-card-image" src="' + imageUrl + '">' +
+            '<md-card-title layout="row" class="layout-row">' +
+            '<md-card-title-text class="lnr-margin-left layout-align-space-around-start layout-column">' +
+            '<span class="md-subhead">' + brand + '</span>' +
+            '<span>' + category + '</span>' +
+            '</md-card-title-text>' +
+            '<div layout="column" class="layout-align-space-around-center layout-column">' +
+            '<span class="md-headline">' + price + ' &euro;</span>' +
+            '<span>per day</span>' +
+            ' </div>' +
+            '</md-card-title>' +
+            '</md-card>' +
+            '</bike-card>' +
+            '</div>'
+          );
         });
       });
     });
   });
 })();
+/*
+
+            '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
+            ride.image_file_1.image_file_1.small.url +
+            '" width="100%" height="80%"></div><div class="mdl-card__supporting-text lnr-margin-left"><span class="md-subhead">' + ride.brand + '</span>' + 
+            ", " + ride.category + "</div></div></div>");
+            */
