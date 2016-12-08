ea8f416a904298d02dd8c3ac6b265c868bb691f4
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-12-08 11:52:56 +0100

    removes testing line from prerender

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 9003));
 app.use(express.static(__dirname + '/listnride/dist'));
-app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000/').set('prerenderToken', 'W8S4Xn73eAaf8GssvVEw'));
+app.use(require('prerender-node').set('prerenderToken', 'W8S4Xn73eAaf8GssvVEw'));
 app.get('/*', function (req, res) {
   res.sendFile(__dirname + '/listnride/dist/index.html');
 });
