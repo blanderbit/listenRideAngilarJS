37e06f9783db1239c8ab726e3ccea2c7a8bd0b09
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2016-12-12 12:49:20 +0100

    intro header, embed bikes.

--- listnride/js_modules/lnr-embed/lnr-embed.js
+++ listnride/js_modules/lnr-embed/lnr-embed.js
@@ -12,46 +12,52 @@
   css_mdl.href = "https://code.getmdl.io/1.2.1/material.blue_grey-blue.min.css";
   css_mdl.rel = "stylesheet";
 
-  var head = document.getElementsByTagName("head")[0];
-  head.appendChild(css_mdl);
-  head.appendChild(css_lnr);
-  head.appendChild(scr_lnr);
+  document.getElementsByTagName("head")[0].appendChild(css_mdl).appendChild(css_lnr).appendChild(scr_lnr);
 
-  var renderBikes = function ($, user_id, rootUrl, grid, categoryFilter) {
-          $.get("https://listnride-staging.herokuapp.com/v2/users/" + user_id, function (response) {
-        response.rides.forEach(function (ride) {
+  var renderBikes = function ($, categoryFilter) {
+    var user_id = document.getElementById('listnride').dataset.user;
+    $.get("https://listnride-staging.herokuapp.com/v2/users/" + user_id, function (response) {
+      $("#listnride")
+        .append('<div class="mdl-grid" id="lnr-grid">' +
+        '<div style="padding:40px;font-size:15px;">We are currently offering the below listed rental bikes in ' + response.city +
+        '. You can easily book your bike rental upfront through our partner website listnride or just come by our shop.' +
+        '</div></div>');
+      
+      var rootUrl = 'http://www.listnride.com';
+      var grid = $("#lnr-grid");
+      
+      response.rides.forEach(function (ride) {
+        var rideId = ride.id,
+          brand = ride.brand,
+          name = ride.name,
+          category = ride.category,
+          categoryDesc = categoryFilter(category),
+          price = parseInt(ride.price_daily),
+          imageUrl = ride.image_file_1.image_file_1.small.url,
+          svgUrl = rootUrl + '/app/assets/ui_icons/biketype_' + (category + '').slice(0, 1) + '.svg';
 
-          var rideId = ride.id,
-            brand = ride.brand,
-            name = ride.name,
-            category = ride.category,
-            categoryDesc = categoryFilter(category),
-            price = parseInt(ride.price_daily),
-            imageUrl = ride.image_file_1.image_file_1.small.url,
-            svgUrl = rootUrl + '/app/assets/ui_icons/biketype_' + (category + '').slice(0, 1) + '.svg';
-
-          grid.append(
-            '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle">' +
-            '<bike-card class="flex-gt-xs-50 flex-gt-sm-33 flex-100">' +
-            '<md-card class="lnr-bike-card _md">' +
-            '<a target="_blank" href="http://www.listnride.com/bikes/' + rideId + '"><img src="' + imageUrl + '"></img></a>' +
-            '<md-card-title layout="row" class="layout-row">' +
-            '<md-icon class="lnr-icn-lrg md-color-foreground" aria-hidden="true"><img src="' + svgUrl + '" height="48" width="48"></img></md-icon>' +
-            '<md-card-title-text class="lnr-margin-left layout-align-space-around-start layout-column">' +
-            '<span class="md-subhead">' + brand + ', ' + categoryDesc + '</span>' +
-            '<span>' + name + '</span>' +
-            '</md-card-title-text>' +
-            '<div layout="column" class="layout-align-space-around-center layout-column">' +
-            '<span class="md-headline">' + price + ' &euro;</span>' +
-            '<span>per day</span>' +
-            ' </div>' +
-            '</md-card-title>' +
-            '</md-card>' +
-            '</bike-card>' +
-            '</div>'
-          );
-        });
+        grid.append(
+          '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle">' +
+          '<bike-card class="flex-gt-xs-50 flex-gt-sm-33 flex-100">' +
+          '<md-card class="lnr-bike-card _md">' +
+          '<a target="_blank" href="http://www.listnride.com/bikes/' + rideId + '"><img src="' + imageUrl + '"></img></a>' +
+          '<md-card-title layout="row" class="layout-row">' +
+          '<md-icon class="lnr-icn-lrg md-color-foreground" aria-hidden="true"><img src="' + svgUrl + '" height="48" width="48"></img></md-icon>' +
+          '<md-card-title-text class="lnr-margin-left layout-align-space-around-start layout-column">' +
+          '<span class="md-subhead">' + brand + ', ' + categoryDesc + '</span>' +
+          '<span>' + name + '</span>' +
+          '</md-card-title-text>' +
+          '<div layout="column" class="layout-align-space-around-center layout-column">' +
+          '<span class="md-headline">' + price + ' &euro;</span>' +
+          '<span>per day</span>' +
+          ' </div>' +
+          '</md-card-title>' +
+          '</md-card>' +
+          '</bike-card>' +
+          '</div>'
+        );
       });
+    });
   }
   var fetchBikesData = function (callback) {
     if (window.jQuery) {
@@ -67,38 +73,57 @@
     $(function () {
       var categoryFilter = function (categoryId) {
         switch (categoryId) {
-          case 10: return "Holland";
-          case 11: return "Touring";
-          case 13: return "Single Speed";
-          case 20: return "Roadbike";
-          case 21: return "Triathlon";
-          case 22: return "Indoor";
-          case 30: return "Trecking";
-          case 31: return "Enduro";
-          case 32: return "Freeride";
-          case 33: return "Cross Country";
-          case 34: return "Downhill";
-          case 35: return "Cyclocross";
-          case 40: return "Children City";
-          case 41: return "Children Allterrain";
-          case 42: return "Children Road";
-          case 50: return "Pedelec";
-          case 51: return "E-Bike";
-          case 60: return "Folding";
-          case 61: return "Tandem";
-          case 62: return "Cruiser";
-          case 63: return "Cargo";
-          case 64: return "Recumbent";
-          case 65: return "Mono";
-          default: return "";
+          case 10:
+            return "Holland";
+          case 11:
+            return "Touring";
+          case 13:
+            return "Single Speed";
+          case 20:
+            return "Roadbike";
+          case 21:
+            return "Triathlon";
+          case 22:
+            return "Indoor";
+          case 30:
+            return "Trecking";
+          case 31:
+            return "Enduro";
+          case 32:
+            return "Freeride";
+          case 33:
+            return "Cross Country";
+          case 34:
+            return "Downhill";
+          case 35:
+            return "Cyclocross";
+          case 40:
+            return "Children City";
+          case 41:
+            return "Children Allterrain";
+          case 42:
+            return "Children Road";
+          case 50:
+            return "Pedelec";
+          case 51:
+            return "E-Bike";
+          case 60:
+            return "Folding";
+          case 61:
+            return "Tandem";
+          case 62:
+            return "Cruiser";
+          case 63:
+            return "Cargo";
+          case 64:
+            return "Recumbent";
+          case 65:
+            return "Mono";
+          default:
+            return "";
         }
       };
-      var id_lnr = $("#listnride");
-      var user_id = document.getElementById('listnride').dataset.user;
-      var rootUrl = 'http://www.listnride.com';
-      id_lnr.append('<div class="mdl-grid" id="lnr-grid"></div>'); 
-      var grid = $("#lnr-grid");
-      renderBikes($, user_id, rootUrl, grid, categoryFilter);
+      renderBikes($, categoryFilter);
     });
   });
 })();
