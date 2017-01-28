9dce89ea938ed2f4955685489a3636ba09b95e36
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-01-28 10:55:05 +0100

    add structure for crossride page

--- listnride/app/app.routes.js
+++ listnride/app/app.routes.js
@@ -138,6 +138,12 @@
       });
 
       $stateProvider.state({
+        name: 'crossride',
+        url: '/crossride',
+        template: '<crossride></crossride>'
+      });
+
+      $stateProvider.state({
         name: 'listingABike',
         url: '/listing-a-bike',
         template: '<listing-a-bike></listing-a-bike>',
