b04c83d716a0f66dd87d91380278841e0eb266cf
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-03-08 17:16:37 +0100

    implemented translations framework for shop solutions wizard. translations and helper class are now in separate files.

---
+++ listnride/js_modules/lnr-embed/lnr-shop-wizard.translations.js
@@ -0,0 +1,149 @@
+var translationsConfigObject = {
+    en: {
+        shared: {
+            lnrHeaderText: 'The perfect bike. Simply rent it.'
+        },
+        tabs: {
+            tabDuration: 'Duration',
+            tabBasicInfo: 'User Info',
+            tabPaymentDetail: 'Payment Details',
+            tabBookingOverview: 'Booking Overview'
+        },
+        durationPanel: {
+            info: 'Please mark rent period:',
+            from: 'from',
+            to: 'to',
+            infoHeader: '',
+            infoText: ''
+        },
+        basicInfoPanel: {
+            info: 'Please fill you personal info:',
+            firstName: 'Firstname',
+            lastName: 'Lastname',
+            email: 'Email Address',
+            infoHeader: '',
+            infoText: ''
+        },
+        paymentDetailsPanel: {
+            cardholderName: 'Cardholder Name',
+            cardNumber: 'Card Number',
+            payPal: 'PayPal',
+            creditCard: 'Credit Card',
+            expiry: 'Expires',
+            CVV: 'CVV',
+            save: 'Save Data',
+            infoHeader: 'Enter your Payment Details',
+            infoText: 'All your data gets transmitted securely via SSH. You can pay by Paypal or Credit Card.'
+        },
+        rental: {
+            duration: 'duration',
+            fee: 'listnride fee (incl. tax)',
+            total: 'total',
+            subtotal: 'subtotal'
+        },
+        bookingOverviewPanel: {},
+        navigation: {
+            back: 'Back',
+            next: 'Next',
+            proceed: 'Proceed'
+        }
+    },
+    de: {
+        shared: {
+            lnrHeaderText: 'The perfect bike. Simply rent it.'
+        },
+        tabs: {
+            tabDuration: 'Duration',
+            tabBasicInfo: 'User Info',
+            tabPaymentDetail: 'Payment Details',
+            tabBookingOverview: 'Booking Overview'
+        },
+        durationPanel: {
+            info: 'Please mark rent period:',
+            from: 'from',
+            to: 'to',
+            infoHeader: '',
+            infoText: ''
+        },
+        basicInfoPanel: {
+            info: 'Please fill you personal info:',
+            firstName: 'Firstname',
+            lastName: 'Lastname',
+            email: 'Email Address',
+            infoHeader: '',
+            infoText: ''
+        },
+        paymentDetailsPanel: {
+            cardholderName: 'Cardholder Name',
+            cardNumber: 'Card Number',
+            payPal: 'PayPal',
+            creditCard: 'Credit Card',
+            expiry: 'Expires',
+            CVV: 'CVV',
+            save: 'Save Data',
+            infoHeader: 'Enter your Payment Details',
+            infoText: 'All your data gets transmitted securely via SSH. You can pay by Paypal or Credit Card.'
+        },
+        rental: {
+            duration: 'duration',
+            fee: 'listnride fee (incl. tax)',
+            total: 'total',
+            subtotal: 'subtotal'
+        },
+        bookingOverviewPanel: {},
+        navigation: {
+            back: 'Back',
+            next: 'Next',
+            proceed: 'Proceed'
+        }
+    },
+    nl: {
+        shared: {
+            lnrHeaderText: 'The perfect bike. Simply rent it.'
+        },
+        tabs: {
+            tabDuration: 'Duration',
+            tabBasicInfo: 'User Info',
+            tabPaymentDetail: 'Payment Details',
+            tabBookingOverview: 'Booking Overview'
+        },
+        durationPanel: {
+            info: 'Please mark rent period:',
+            from: 'from',
+            to: 'to',
+            infoHeader: '',
+            infoText: ''
+        },
+        basicInfoPanel: {
+            info: 'Please fill you personal info:',
+            firstName: 'Firstname',
+            lastName: 'Lastname',
+            email: 'Email Address',
+            infoHeader: '',
+            infoText: ''
+        },
+        paymentDetailsPanel: {
+            cardholderName: 'Cardholder Name',
+            cardNumber: 'Card Number',
+            payPal: 'PayPal',
+            creditCard: 'Credit Card',
+            expiry: 'Expires',
+            CVV: 'CVV',
+            save: 'Save Data',
+            infoHeader: 'Enter your Payment Details',
+            infoText: 'All your data gets transmitted securely via SSH. You can pay by Paypal or Credit Card.'
+        },
+        rental: {
+            duration: 'duration',
+            fee: 'listnride fee (incl. tax)',
+            total: 'total',
+            subtotal: 'subtotal'
+        },
+        bookingOverviewPanel: {},
+        navigation: {
+            back: 'Back',
+            next: 'Next',
+            proceed: 'Proceed'
+        }
+    },
+}
