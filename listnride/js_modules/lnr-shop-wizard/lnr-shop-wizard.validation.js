170805cd8b55f030332be6e0e1e4006c3897492f
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-03-18 19:15:50 +0100

    Updated validations

--- listnride/js_modules/lnr-shop-wizard/lnr-shop-wizard.validation.js
+++ listnride/js_modules/lnr-shop-wizard/lnr-shop-wizard.validation.js
@@ -1,23 +1,42 @@
 $(function () {
     $('#user-info-validation').addClass('hidden');
-    $('#user-info').on('click', function () {
-        $('#user-info-validation').toggleClass('hidden', $('#user-info md-input-container.is-invalid').length == 0)
-    })
+    $('#user-info').on('keyup', function () {
+        if ($('#user-info md-input-container.is-invalid').length == 0) {
+            $('#user-info-validation').addClass('hidden')
+        } else {
+            $('#user-info-validation').removeClass('hidden')
+        }
+    });
+    $('#form_first_name').blur(function(){ validateField('#form_first_name', '#user')});
+    $('#form_last_name').blur(function(){ validateField('#form_last_name', '#user')});
+    $('#form_email').blur(function(){ validateField('#form_email', '#user')});
+    $('#form_email_repeat').blur(function(){
+        if ($('#form_email_repeat') != $('#form_email_repeat').val) $('#form_email_repeat').parent().addClass('is-invalid');
+        validateField('#form_email_repeat', '#user')
+    });
 });
+
 
 $(function () {
     $('#payment-info-validation').addClass('hidden');
-    $('#sp-payment-form .mdl-grid').on('click', function () {
+    $('#sp-payment-form').on('keyup', function () {
         $('#payment-info-validation').toggleClass('hidden', $('#sp-payment-form md-input-container.is-invalid').length == 0);
     });
 
-    // $('#lnr-payment-date-button').on('change', function () {
-    //     $('#payment-info-validation').toggleClass('hidden', $('#lnr-payment-date-button').text() != "Month" );
-    // });
-    // $('#lnr-payment-year-button').on('change', function () {
-    //     $('#payment-info-validation').toggleClass('hidden', $('#lnr-payment-year-button').text() != "Year" );
-    // });
+    $('#sp-payment-cardholder').blur(function(){ validateField('#sp-payment-cardholder', '#payment')});
+    $('#sp-payment-cardnumber').blur(function(){ validateField('#sp-payment-cardnumber', '#payment')});
+    $('#sp-payment-cvv').blur(function(){ validateField('#sp-payment-cvv', '#payment')});
 });
+
+function validateField(input_id, text_id) {
+    if ($(input_id).parent().hasClass('is-invalid') || $(input_id).val().length == 0) {
+        $(input_id).parent().addClass('is-invalid');
+        $(input_id).closest("div").find("label").addClass('text-invalid');
+        $(text_id + '-info-validation').removeClass('hidden');
+    } else {
+        $(text_id + '-info-validation').addClass('hidden');
+    }
+}
 
 MaterialTextfield.prototype.checkValidity = function () {
     if (this.input_.validity.valid) {
