7583eb9a13f142455dc900b67d123756f4450035
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-03-23 16:47:57 +0200

    Updated price showing

--- listnride/app/modules/shared/bike-card/bike-card.component.js
+++ listnride/app/modules/shared/bike-card/bike-card.component.js
@@ -9,6 +9,9 @@ angular.module('bikeCard',[]).component('bikeCard', {
   controller: ['$mdMedia',
     function BikeCardController($mdMedia) {
       var bikeCard = this;
+      var hal_daily = parseInt(bikeCard.bike.price_weekly)
+      var price_weekly = parseInt(bikeCard.bike.price_half_daily)
+      bikeCard.from = Math.min(hal_daily, price_weekly);
       bikeCard.isPhoneScreen = $mdMedia('xs');
     }
   ]
