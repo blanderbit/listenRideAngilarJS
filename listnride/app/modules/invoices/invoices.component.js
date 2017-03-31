f42f4fc169deb2edf86de504e4dfbcc793d0b599
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-03-31 18:17:47 +0300

    Updates for Billing page
    - Infinite scroll
    - Years display
    - Working CSV button

--- listnride/app/modules/invoices/invoices.component.js
+++ listnride/app/modules/invoices/invoices.component.js
@@ -9,16 +9,58 @@ angular.module('invoices',[]).component('invoices', {
                 return
             }
             var invoices = this;
+            invoices.ridesAsListerAny = false;
+            invoices.ridesAsRiderAny = false;
 
-            // api.get('/users/' + $localStorage.userId + "/reports").then(
-            //     function(response) {
-            //         invoices.asLister = response.data.as_lister;
-            //         invoices.asRider = response.data.as_rider;
-            //     },
-            //     function(error) {
-            //         console.log("Error retrieving User", error);
-            //     }
-            // );
+            api.get('/users/' + $localStorage.userId + "/reports").then(
+                function(response) {
+                    invoices.asLister = response.data.as_lister;
+                    invoices.asRider = response.data.as_rider;
+                    invoices.years = Object.keys(invoices.asRider).reverse();
+                    invoices.ridesAny('rider');
+                    invoices.ridesAny('lister');
+                },
+                function(error) {
+                    console.log("Error retrieving User", error);
+                }
+            );
+
+            invoices.parseDate = function (milliseconds) {
+                var monthNames = ["Jan", "Feb", "Mar",
+                    "Apr", "May", "Jun", "Jul",
+                    "Aug", "Sep", "Oct",
+                    "Nov", "Dec"];
+                var date = new Date(milliseconds);
+                var day = date.getDate();
+                var hours = date.getHours();
+                var ampm = hours >= 12 ? 'pm' : 'am';
+                var monthIndex = date.getMonth();
+                return day + ' ' + monthNames[monthIndex] + ' - ' + hours + ' ' + ampm
+            };
+
+            invoices.getCsv = function (target) {
+                api.get('/users/' + $localStorage.userId + "/transaction_csv?target=" + target).then(
+                    function(response) {
+                        var anchor = angular.element('<a/>');
+                        anchor.attr({
+                            href: 'data:attachment/csv;charset=utf-8,' + encodeURI(response.data),
+                            target: '_blank',
+                            download: 'Billings as ' + target + '' + moment().format('MMMM Do YYYY') + '.csv'
+                        })[0].click();
+                    },
+                    function(error) {
+                        console.log("Error retrieving CSV", error);
+                    }
+                );
+            };
+
+            invoices.ridesAny = function(target) {
+                if (target == 'rider') {
+                    invoices.ridesAsRiderAny = !_.isEmpty(invoices.asRider)
+                } else {
+                    invoices.ridesAsListerAny = !_.isEmpty(invoices.asLister)
+                }
+            };
 
             invoices.items = [];
             for (var i = 0; i < 100001; i++) {
