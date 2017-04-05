97dfe8df8aa99063e754e2cee66911a2e6efc659
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-04-05 15:55:48 +0200

    first optimizations in controller

--- listnride/app/modules/brand-integration/ampler.component.js
+++ listnride/app/modules/brand-integration/ampler.component.js
@@ -9,18 +9,26 @@ angular.module('brand-integration',[]).component('ampler', {
 
       ampler.bikesBerlin = [];
       ampler.bikesMunich = [];
+      ampler.bikesHamburg = [];
+      ampler.bikesVienna = [];
 
       api.get('/rides?family=8').then(
         function (success) {
           console.log(success.data);
 
           for (var i=0; i<success.data.length; i++) {
-            if (success.data[i].city == "Berlin") {
-              ampler.bikesBerlin.push(success.data[i]);
+            switch (success.data[i].city) {
+              case "Berlin": ampler.bikesBerlin.push(success.data[i]);
+              case "Munich": ampler.bikesBerlin.push(success.data[i]);
+              case "Hamburg": ampler.bikesHamburg.push(success.data[i]);
+              case "Vienna": ampler.bikesVienna.push(success.data[i]);
             }
-            else {
-              ampler.bikesMunich.push(success.data[i]);
-            }
+            // if (success.data[i].city == "Berlin") {
+            //   ampler.bikesBerlin.push(success.data[i]);
+            // }
+            // else if (success.data[i].city{
+            //   ampler.bikesMunich.push(success.data[i]);
+            // }
           }
         },
         function (error) {
