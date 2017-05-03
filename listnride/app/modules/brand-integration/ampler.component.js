76429e5d466dcae6af6876715ba7e3cfe858fe62
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-05-03 16:18:53 +0200

    add testimonials to ampler page

--- listnride/app/modules/brand-integration/ampler.component.js
+++ listnride/app/modules/brand-integration/ampler.component.js
@@ -21,6 +21,45 @@ angular.module('ampler-integration',[]).component('ampler', {
         hamburg: [],
         vienna: []
       };
+      ampler.slickConfig = {
+        enabled: true,
+        autoplay: true,
+        draggable: true,
+        autoplaySpeed: 12000,
+        ease: 'ease-in-out',
+        speed: '500',
+        prevArrow: "<img class='testimonials-prev-arrow slick-prev' src='app/assets/ui_images/back.png'>",
+        nextArrow: "<img class='testimonials-prev-arrow slick-next' src='app/assets/ui_images/next.png'>"
+      };
+
+      $translate('shared.from-place').then(function(translation) {
+        ampler.testimonials = [
+          {
+            userId: 1938,
+            userName: "Robert " + translation + " Dresden",
+            userImagePath: "app/assets/ui_images/brand-integration/ampler_testimonial_1.jpg",
+            text: "Geiles, schnelles Bike. Ging ab wie Schmidts Katze. Übergabe verlief völlig unkompliziert und sehr nett."
+          },
+          {
+            userId: 1739,
+            userName: "Cornelia " + translation + " Binningen",
+            userImagePath: "app/assets/ui_images/brand-integration/ampler_testimonial_2.jpg",
+            text: "Sehr unkomplizierte und angenehme Art. Es hat alles bestens geklappt. Das Ampler fährt sich sehr gut, freuen uns auf das nächste Mal."
+          },
+          {
+            userId: 1775,
+            userName: "Marek " + translation + " Berlin",
+            userImagePath: "app/assets/ui_images/brand-integration/ampler_testimonial_3.jpg",
+            text: "ein sehr geiles Bike und eine krasse Erfahrung, das erste Mal ein Ebike zu fahren. könnte mir sogar vorstellen sowas zu kaufen :)"
+          },
+          {
+            userId: 1727,
+            userName: "Thomas " + translation + " Münster",
+            userImagePath: "app/assets/ui_images/brand-integration/ampler_testimonial_4.jpg",
+            text: "Hat alles gepasst! ein sehr schönes Rad und ein Vergnügen zum Fahren. Kann es jeder sehr empfehlen mal zu Probefahren, macht wirklich Spaß ;)"
+          }
+        ];
+      });
 
       api.get('/rides?family=8').then(
         function (success) {
