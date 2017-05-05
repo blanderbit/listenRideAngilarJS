5dc41f6103faa918410251b9f870bbd9098c051d
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-05-05 17:58:16 +0200

    Updated meta tags to work with preload

--- listnride/app/modules/events/velothon-bikerental/velothon-bikerental.component.js
+++ listnride/app/modules/events/velothon-bikerental/velothon-bikerental.component.js
@@ -3,9 +3,12 @@
 angular.module('velothonBikerental',[]).component('velothonBikerental', {
     templateUrl: 'app/modules/events/velothon-bikerental/velothon-bikerental.template.html',
     controllerAs: 'velothonBikerental',
-    controller: ['NgMap', 'api',
-        function VelothonBikerental(NgMap, api) {
+    controller: ['NgMap', 'api', '$translate', 'ngMeta',
+        function VelothonBikerental(NgMap, api, $translate, ngMeta) {
             var velothonBikerental = this;
+
+            ngMeta.setTitle($translate.instant("events.velothon-bikerental.meta-title"));
+            ngMeta.setTag("description", $translate.instant("events.velothon-bikerental.meta-description"));
 
             api.get('/rides?category=20&location=Berlin&priority=velothon').then(
                 function(response) {
