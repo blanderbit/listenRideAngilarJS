493cedbc6707cf5616913badb4069cfdb6928bc0
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-05-07 10:56:23 +0200

    add referral overview

--- listnride/app/modules/invite/invite.component.js
+++ listnride/app/modules/invite/invite.component.js
@@ -3,15 +3,21 @@
 angular.module('invite',[]).component('invite', {
   templateUrl: 'app/modules/invite/invite.template.html',
   controllerAs: 'invite',
-  controller: [
-    function InviteController() {
+  controller: ['api', '$localStorage', 
+    function InviteController(api, $localStorage) {
       console.log("hi", invite);
       var invite = this;
       invite.inviteUrl = "http://www.listnride.com/invitation/amrXXX69";
 
       invite.onCopyButtonClick = function() {
-        
+
       };
+
+      api.get("/users/" + $localStorage.userId + "/overview ").then(function(response) {
+        invite.invitedFriends = response.data;
+        invite.totalCredit = invite.invitedFriends.filter(function(invitation) {return invitation.status}).length * 10;
+        console.log("invite.invitedFriends", invite.invitedFriends, invite.totalCredit);
+      });
 
 /*      ngMeta.setTitle($translate.instant("home.meta-title"));
       ngMeta.setTag("description", $translate.instant("home.meta-description"));
