58e0de65fc68bcc6aab6fbc6d6af8fdaa1ce3583
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-05-07 14:44:16 +0200

    finish up page

--- listnride/app/modules/invite-landing/invite-landing.component.js
+++ listnride/app/modules/invite-landing/invite-landing.component.js
@@ -4,16 +4,20 @@ angular.module('inviteLanding',[]).component('inviteLanding', {
   templateUrl: 'app/modules/invite-landing/invite-landing.template.html',
   controllerAs: 'inviteLanding',
   controller: [ '$stateParams', '$state', 'authentication', 'api',
-    function InviteLandingController($stateParams, $state, authentication, api) {
+    function InviteLandingController($stateParams, $state, authentication, api, referrer) {
       var inviteLanding = this;
 
+      inviteLanding.hidden = true;
       inviteLanding.authentication = authentication;
       inviteLanding.inviteCode = $stateParams.inviteCode;
-      inviteLanding.referrer = "";
 
       api.get('/referrals/' + inviteLanding.inviteCode).then(
         function (success) {
-          inviteLanding.referrer = success.data.first_name
+          inviteLanding.referrer = success.data;
+          console.log(inviteLanding.referrer);
+          inviteLanding.picture = inviteLanding.referrer.profile_picture.profile_picture.url;
+          console.log(inviteLanding.picture);
+          inviteLanding.hidden = false;
         },
         function (error) {
           $state.go("404");
