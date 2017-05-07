94876b23c224db5b6f775752855e3c8750bdbe66
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-05-07 09:56:07 +0200

    content updates for landing page

--- listnride/app/modules/invite-landing/invite-landing.component.js
+++ listnride/app/modules/invite-landing/invite-landing.component.js
@@ -3,12 +3,23 @@
 angular.module('inviteLanding',[]).component('inviteLanding', {
   templateUrl: 'app/modules/invite-landing/invite-landing.template.html',
   controllerAs: 'inviteLanding',
-  controller: [ '$stateParams', 'authentication',
-    function InviteLandingController($stateParams, authentication) {
+  controller: [ '$stateParams', '$state', 'authentication', 'api',
+    function InviteLandingController($stateParams, $state, authentication, api) {
       var inviteLanding = this;
 
       inviteLanding.authentication = authentication;
       inviteLanding.inviteCode = $stateParams.inviteCode;
+      inviteLanding.referrer = "";
+
+      api.get('/referrals/' + inviteLanding.inviteCode).then(
+        function (success) {
+          inviteLanding.referrer = success.data.first_name
+        },
+        function (error) {
+          $state.go("404");
+          console.log("an error happened ERR");
+        }
+      );
 
       inviteLanding.showSignup = function() {
         authentication.showSignupDialog(inviteCode);
