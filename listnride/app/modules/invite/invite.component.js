f86c9b82612dfac1b423b404e7d87abdc8f13cec
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-05-07 16:49:36 +0200

    add invitations over email

--- listnride/app/modules/invite/invite.component.js
+++ listnride/app/modules/invite/invite.component.js
@@ -3,20 +3,37 @@
 angular.module('invite',[]).component('invite', {
   templateUrl: 'app/modules/invite/invite.template.html',
   controllerAs: 'invite',
-  controller: ['api', '$localStorage', 
-    function InviteController(api, $localStorage) {
+  controller: ['api', '$localStorage', '$translate', 
+    function InviteController(api, $localStorage, $translate) {
       var invite = this;
       invite.inviteUrl = "http://www.listnride.com/invitation/amrXXX69";
 
-      invite.onCopyButtonClick = function() {
+      invite.copyToClipboard = function() {
         document.getElementById("linkContainer").select();
         document.execCommand('copy');
       };
 
+      invite.shareThroughFacebook = function() {
+
+      };
+
+      invite.shareThroughEmail = function() {
+        var userFullName = $localStorage.name;
+        var voucherUrl = invite.inviteUrl;
+        $translate(
+          ["invite.share.email-subject", "invite.share.email-body"],
+          {full_name: userFullName, voucher_url: voucherUrl})
+          .then(function (translations) {
+            var subject = translations["invite.share.email-subject"];
+            var body = translations["invite.share.email-body"];
+            window.open("mailto:" + "?subject=" + subject + "&body=" + body, "_self");
+          })
+      };   
+
       api.get("/users/" + $localStorage.userId + "/overview ").then(function(response) {
         invite.invitedFriends = response.data;
-        invite.totalCredit = invite.invitedFriends.filter(function(invitation) {return invitation.status}).length * 10;
-        console.log("invite.invitedFriends", invite.invitedFriends, invite.totalCredit);
+        var activeFriends = invite.invitedFriends.filter(function(invitation) {return invitation.status});
+        invite.totalCredit = activeFriends.length * 10;
       });
     }
   ]
