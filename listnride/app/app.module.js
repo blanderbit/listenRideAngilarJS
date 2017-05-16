4e3ea1d49339e9eb96dcbd403ef664353c1eaee8
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-05-16 12:08:33 +0200

    setup facebook pixel code and events

--- listnride/app/app.module.js
+++ listnride/app/app.module.js
@@ -51,7 +51,7 @@ angular.module('listnride', [
   'slickCarousel',
   'angulartics',
   'angulartics.google.analytics',
-  'angulartics-facebook-pixel',
+  'angulartics.facebook.pixel',
   'angularMoment'
 ])
 .config(['$translateProvider', 'ezfbProvider', '$mdAriaProvider', '$locationProvider', 'ngMetaProvider', 'ENV',
