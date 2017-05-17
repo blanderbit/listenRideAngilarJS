1a526e1761fb85102b2f3619f6c046273e05c4cb
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-05-17 14:53:53 +0200

    deactivate invite system links and routes

--- listnride/app/app.routes.js
+++ listnride/app/app.routes.js
@@ -583,17 +583,18 @@
         templateUrl: 'app/modules/static/how-to-shoot-bike-photos.template.html'
       });
 
-      $stateProvider.state({
-        name: 'invite',
-        url: '/invite-friends',
-        template: '<invite></invite>'
-      });
+      // Temporarily deactivated
+      // $stateProvider.state({
+      //   name: 'invite',
+      //   url: '/invite-friends',
+      //   template: '<invite></invite>'
+      // });
 
-      $stateProvider.state({
-        name: 'inviteLanding',
-        url: '/invitation/{inviteCode: string}',
-        template: '<invite-landing></invite-landing>'
-      });
+      // $stateProvider.state({
+      //   name: 'inviteLanding',
+      //   url: '/invitation/{inviteCode: string}',
+      //   template: '<invite-landing></invite-landing>'
+      // });
 
       $stateProvider.state('404', {
         templateUrl: 'app/modules/static/error-404.template.html',
