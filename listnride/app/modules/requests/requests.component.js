3f38e63d1d4cb701ad142eb6304b2486890c9f3b
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-06-08 13:26:53 +0200

    optimizations for the notification bubble

--- listnride/app/modules/requests/requests.component.js
+++ listnride/app/modules/requests/requests.component.js
@@ -118,9 +118,10 @@ angular.module('requests', []).component('requests', {
         reloadRequest(requestId);
         api.post('/requests/' + requestId + '/messages/mark_as_read', { "user_id": $localStorage.userId }).then(
           function (success) {
-            if (index) {
+            // if (index) {
               requests.requests[index].last_message.is_read = true;
-            }
+              $localStorage.unreadMessages -= 1;
+            // }
           }
         );
         poller = $interval(function () {
