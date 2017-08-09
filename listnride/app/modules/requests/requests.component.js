e76bfddd4c798690284850a27f983b54515644da
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-08-09 14:48:42 +0200

    disable return confirmation before rental ended

--- listnride/app/modules/requests/requests.component.js
+++ listnride/app/modules/requests/requests.component.js
@@ -161,6 +161,7 @@ angular.module('requests', []).component('requests', {
               requests.request = success.data;
               requests.request.rideChat = $localStorage.userId == requests.request.user.id;
               requests.request.rideChat ? requests.request.chatFlow = "rideChat" : requests.request.chatFlow = "listChat";
+              requests.request.past = (new Date(requests.request.end_date).getTime() < Date.now());
 
               if (requests.request.rideChat) {
                 requests.request.rating = requests.request.lister.rating_lister + requests.request.lister.rating_rider;
