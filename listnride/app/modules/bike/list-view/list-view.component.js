b36bb91fea03e340d4f79401f823c9fceac2a613
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-09-30 14:58:24 +0200

    select element behaviour

---
+++ listnride/app/modules/bike/list-view/list-view.component.js
@@ -0,0 +1,58 @@
+angular.module('bikeListView', []).component('bikeListView', {
+  templateUrl: 'app/modules/bike/list-view/list-view.template.html',
+  controllerAs: 'list',
+  controller: ['$stateParams', 'api', 'bikeOptions', function ($stateParams, api, bikeOptions) {
+    var list = this;
+
+    api.get('/rides/').then(
+      function (response) {
+        list.bikes = response.data.slice(10, 20);
+        list.selected = [];
+        list.hovered = [];
+        list.visibility = [];
+        list.listElementBackground = [];
+        list.selectedCount = 0;
+      },
+      function (error) {
+        console.log("Error editing bike", error);
+      }
+    );
+
+    // bike checkbox is selected
+    list.onBikeSelected = function (index) {
+      if (list.selected[index]) {
+        // clear the hover status of the item
+        list.hovered = [];
+        // hide the action buttons for the item
+        list.visibility[index] = false;
+        // set background for the item
+        list.listElementBackground[index] = {'background-color': 'rgba(158, 158, 158, 0.2)'}
+      } else {
+        list.listElementBackground = [];
+      }
+    };
+
+    // check if a bike checkbox is selected
+    list.isBikeSelected = function (threshold) {
+      if (list.selected.filter(function (isTrue) {
+          return isTrue;
+        }).length > threshold) {
+        return true;
+      }
+    };
+
+    //
+    list.onBikeHovered = function (index, value) {
+      if (!list.isBikeSelected(0)) {
+        list.hovered[index] = value;
+        // used in place of ng-show
+        // using visibility instead of display for hiding/showing the elements
+        list.visibility[index] = value === true ? {'visibility': 'visible'} : {'visibility': 'hidden'};
+      }
+    };
+
+    list.editBike = function (event) {
+      console.log("$event: ", event);
+    };
+  }]
+});
