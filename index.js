67244074842f4074c4d25ea767c11efdee68df9e
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-11-24 19:38:42 +0100

    activate static files.

--- index.js
+++ index.js
@@ -63,5 +63,19 @@ app.all('/', function(req, res, next) {
 // http://expressjs.com/en/4x/api.html#express.static
 app.use(express.static(__dirname.concat('/listnride/dist'), {index: 'index.html'}));
 
+/*
+removing this will disable serving urls from browser
+
+it will only be called when there is some url
+otherwise app.use(express.static ...) will be called
+
+sometimes it will get called even on root in case of chrome
+that is because 'angular-sanitize.min.js.map' is missing
+and chrome requests it. not for safari and firefox
+*/
+app.use('/*', function (req, res) {
+  res.sendFile(__dirname.concat('/listnride/dist/index.html'));
+});
+
 app.listen(app.get('port'), function () {
 });
