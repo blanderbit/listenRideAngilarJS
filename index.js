e432a76e71f7c4bd3c3d07352a59fa39eb32afac
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-11-24 17:44:58 +0100

    DEV - reseting domain level redirections -- still not fixed :(

--- index.js
+++ index.js
@@ -48,22 +48,35 @@ var retrieveTld = function(hostname) {
   return hostname.replace(/^(.*?)\listnride/, "");
 }
 
-var redirectUrl = function (req, res, next) {
-  var correctHostname = stripTrailingSlash(determineHostname(req.subdomains, req.hostname));
-  var correctOriginalUrl = stripTrailingSlash(req.originalUrl);
-  if (req.hostname === correctHostname && req.originalUrl === correctOriginalUrl) {
-  } else {
-    res.redirect(301, 'https://www.staging.listnride.de'); // "https://" + correctHostname + correctOriginalUrl
-  }
-  return next();
-};
-
-// proper redirects
-app.all('/', function(req, res, next) {redirectUrl(req, res, next)});
+// // proper redirects
+// app.use(function(req, res, next) {
+//   var correctHostname = stripTrailingSlash(determineHostname(req.subdomains, req.hostname));
+//   var correctOriginalUrl = stripTrailingSlash(req.originalUrl);
+//   if (req.hostname === correctHostname && req.originalUrl === correctOriginalUrl) {
+//     next();
+//   } else {
+//     res.redirect(301, "https://" + correctHostname + correctOriginalUrl);
+//   }
+// });
 
 // by default serves index.html
 // http://expressjs.com/en/4x/api.html#express.static
 app.use(express.static(__dirname.concat('/listnride/dist'), {index: 'index.html'}));
+
+/*
+removing this will disable serving urls from browser
+
+it will only be called when there is some url
+otherwise app.use(express.static ...) will be called
+
+sometimes it will get called even on root in case of chrome
+that is because 'angular-sanitize.min.js.map' is missing
+and chrome requests it. not for safari and firefox
+*/
+app.use('/*', function (req, res) {
+
+  res.sendFile(__dirname.concat('/listnride/dist/index.html'));
+});
 
 app.listen(app.get('port'), function () {
 });
