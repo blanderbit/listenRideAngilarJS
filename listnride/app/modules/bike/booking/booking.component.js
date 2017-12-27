388398ef3b9efd635cca270de9ff351b2cda54af
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-12-27 09:14:09 +0100

    new request flow -- tabs components

--- listnride/app/modules/bike/booking/booking.component.js
+++ listnride/app/modules/bike/booking/booking.component.js
@@ -1,21 +1,28 @@
 'use strict'
 
-// TODO: make it part of bike module. No separate module required
-angular.module('booking', []).component('booking', {
-  templateUrl: 'app/modules/bike/booking/booking.template.html',
-  controllerAs: 'booking',
-  controller: ['authentication', function BookingController(authentication) {
-    var booking = this;
+angular.module('booking', [])
+  // personal tab ui component
+  .component('personalTab', {templateUrl: 'app/modules/bike/booking/personal-tab.template.html'})
+  // payment tab ui component
+  .component('paymentTab', {templateUrl: 'app/modules/bike/booking/payment-tab.template.html'})
+  // overview tab ui component
+  .component('overviewTab', {templateUrl: 'app/modules/bike/booking/overview-tab.template.html'})
+  // booking component
+  .component('booking', {
+    templateUrl: 'app/modules/bike/booking/booking.template.html',
+    controllerAs: 'booking',
+    controller: ['authentication', function BookingController(authentication) {
+      var booking = this;
 
-    // on lifecycle initialization
-    booking.$onInit = function () {
-      booking.authentication = authentication;
-      booking.showConfirmButton = true;
-    };
+      // on lifecycle initialization
+      booking.$onInit = function () {
+        booking.authentication = authentication;
+        booking.showConfirmButton = true;
+      };
 
-    // toggle confirm phone button
-    booking.toggleConfirmButton = function(){
-      booking.showConfirmButton = !booking.showConfirmButton;
-    }
-  }]
-});
+      // toggle confirm phone button
+      booking.toggleConfirmButton = function () {
+        booking.showConfirmButton = !booking.showConfirmButton;
+      }
+    }]
+  })
