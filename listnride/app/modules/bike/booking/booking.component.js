87d355d4b18890c1a03815a7514d9f9b1b64baef
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2017-12-27 10:33:25 +0100

    request flow -- navigations

--- listnride/app/modules/bike/booking/booking.component.js
+++ listnride/app/modules/bike/booking/booking.component.js
@@ -13,29 +13,40 @@ angular.module('booking', [])
       booking.$onInit = function () {
         booking.authentication = authentication;
         booking.showConfirmButton = true;
+        booking.selectedIndex = 0;
       };
 
       // toggle confirm phone button
       booking.toggleConfirmButton = function () {
         booking.showConfirmButton = !booking.showConfirmButton;
       }
+
+      // go to next tab
+      booking.nextTab = function () {
+        booking.selectedIndex = booking.selectedIndex + 1;
+      };
+
+      // go to previous tab
+      booking.previousTab = function () {
+        booking.selectedIndex = booking.selectedIndex - 1;
+      };
     }]
   })
   // personal tab ui component
   .component('personalTab', {
     templateUrl: 'app/modules/bike/booking/personal-tab.template.html',
-    require: { booking: '^booking' },
+    require: { parent: '^booking' },
     controllerAs: 'personal'
   })
   // payment tab ui component
   .component('paymentTab', {
     templateUrl: 'app/modules/bike/booking/payment-tab.template.html',
-    require: { booking: '^booking' },
+    require: { parent: '^booking' },
     controllerAs: 'payment'
   })
   // overview tab ui component
   .component('overviewTab', {
     templateUrl: 'app/modules/bike/booking/overview-tab.template.html',
-    require: { booking: '^booking' },
+    require: { parent: '^booking' },
     controllerAs: 'overview'
   })
