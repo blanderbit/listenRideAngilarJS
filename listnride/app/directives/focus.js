cb06f5dd4b0c124d7b62ea3fdb883d3a36676ee0
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-02-05 17:05:14 +0200

    Updated sms input behavior

--- listnride/app/directives/focus.js
+++ listnride/app/directives/focus.js
@@ -2,18 +2,20 @@ angular.module('list').directive('focus', function() {
   return {
     restrict: "A",
     link: function($scope, element) {
-      element.on("keydown", function(e) {
+      element.on("keyup", function(e) {
         var input = element.find('input');
+        var key = e.keyCode || e.charCode;
+
         if(input[0].value.length == input.attr("maxlength")) {
           var $nextElement = element.next();
-          var key = e.keyCode || e.charCode;
-          if (key === 8 || key === 46) {
-            return
-          }
-
           if ($nextElement.length) {
             $nextElement.find('input').focus();
           }
+        }
+
+        if (key === 8 || key === 46) {
+          var $prevElement = element.prev();
+          $prevElement.find('input').focus();
         }
       });
     }
