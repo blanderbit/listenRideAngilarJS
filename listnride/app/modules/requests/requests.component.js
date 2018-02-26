ccb1f15732ef0d4fe0e1ef66a97763636ef06840
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-02-26 13:58:56 +0100

    fix seo bug and various others

--- listnride/app/modules/requests/requests.component.js
+++ listnride/app/modules/requests/requests.component.js
@@ -337,6 +337,7 @@ angular.module('requests', ['infinite-scroll'])
         var payoutDialog = this;
 
         payoutDialog.user = user;
+        payoutDialog.emailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         payoutDialog.addPayoutMethod = function () {
           var data = {
