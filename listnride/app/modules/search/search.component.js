9cd5b011cac56ab244b6d0c22b1d80b623defd0e
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-03-11 17:49:19 +0100

    bug fixes in bikes for search

--- listnride/app/modules/search/search.component.js
+++ listnride/app/modules/search/search.component.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('search',[]).component('search', {
+angular.module('search', []).component('search', {
   templateUrl: 'app/modules/search/search.template.html',
   controllerAs: 'search',
   bindings: {
@@ -9,6 +9,37 @@ angular.module('search',[]).component('search', {
   controller: ['$translate', '$http', '$stateParams', '$state', 'NgMap', 'ngMeta', 'api',
     function SearchController($translate, $http, $stateParams, $state, NgMap, ngMeta, api) {
       var search = this;
+
+      function populateBikes(location) {
+        api.get("/rides?location=" + location).then(function (response) {
+          search.bikes = response.data;
+
+          if (search.bikes.length > 0) {
+            search.mapOptions.lat = search.bikes[0].lat_rnd;
+            search.mapOptions.lng = search.bikes[0].lng_rnd;
+            search.mapOptions.zoom = 10;
+          } else {
+            search.mapOptions.lat = 51.1657;
+            search.mapOptions.lng = 10.4515;
+            search.mapOptions.zoom = 4;
+          }
+        }, function (error) {
+          search.bikes = undefined;
+        });
+      }
+
+      function setMetaTags(location) {
+        var data = {
+          location: location
+        };
+        $translate("search.meta-title", data).then(
+          function (translation) {
+            ngMeta.setTitle(translation);
+          }
+        );
+        ngMeta.setTitle($translate.instant("search.meta-title", data));
+        ngMeta.setTag("description", $translate.instant("search.meta-description", data));
+      }
 
       search.location = $stateParams.location;
 
@@ -28,12 +59,12 @@ angular.module('search',[]).component('search', {
       };
 
       search.sizeOptions = [
-        {value: "", label: "-"},
-        {value: 155, label: "155 - 165"},
-        {value: 165, label: "165 - 175"},
-        {value: 175, label: "175 - 185"},
-        {value: 185, label: "185 - 195"},
-        {value: 195, label: "195 - 205"}
+        { value: "", label: "-" },
+        { value: 155, label: "155 - 165" },
+        { value: 165, label: "165 - 175" },
+        { value: 175, label: "175 - 185" },
+        { value: 185, label: "185 - 195" },
+        { value: 195, label: "195 - 205" }
       ];
 
       $translate('search.all-sizes').then(function (translation) {
@@ -48,95 +79,96 @@ angular.module('search',[]).component('search', {
 
       populateBikes(search.location);
 
-      NgMap.getMap({id: "searchMap"}).then(function(map) {
-        search.map = map;
-      });
+      search.initializeGoogleMap = function () {
+        NgMap.getMap({ id: "searchMap" }).then(function (map) {
+          search.map = map;
+        });
+      };
 
-      search.showBikeWindow = function(event, bikeId) {
+      search.showBikeWindow = function (event, bikeId) {
         if (search.map) {
-          search.selectedBike = search.bikes.find(function(bike) {
+          search.selectedBike = search.bikes.find(function (bike) {
             return bike.id === bikeId;
           });
           search.map.showInfoWindow('searchMapWindow', this);
         }
       };
 
-      search.placeChanged = function(place) {
+      search.placeChanged = function (place) {
         var location = place.formatted_address || place.name;
-        $state.go('.', {location: location}, {notify: false});
+        $state.go(
+          // current state
+          $state.current,
+          // state params
+          { location: location },
+          // route options
+          // do not remove inherit prop, else map tiles stop working
+          { notify: false }
+        );
         search.location = location;
         setMetaTags(location);
         populateBikes(location);
       };
 
-      search.onButtonClick = function() {
-        $state.go('.', {location: search.location}, {notify: false});
+      search.onButtonClick = function () {
+        $state.go(
+          // current state
+          $state.current,
+          // state params
+          { location: search.location },
+          // route options
+          // do not remove inherit prop, else map tiles stop working
+          { notify: false }
+        );
         populateBikes(search.location);
       };
 
-      search.onSizeChange = function() {
-        $state.go('.', {size: search.sizeFilter.size}, {notify: false});
+      search.onSizeChange = function () {
+        $state.go(
+          // current state
+          $state.current,
+          // state params
+          { size: search.sizeFilter.size },
+          // route options
+          // do not remove inherit prop, else map tiles stop working
+          { notify: false }
+        );
       };
 
-      search.onCategoryChange = function(category) {
+      search.onCategoryChange = function (category) {
         var categoryMap = {};
         categoryMap[category] = search.categoryFilter[category];
-        $state.go('.', categoryMap, {notify: false});
+        $state.go(
+          // current state
+          $state.current,
+          // state params
+          categoryMap,
+          // route options
+          // do not remove inherit prop, else map tiles stop working
+          { notify: false }
+        );
       };
 
-      search.onMapClick = function(event) {
+      search.onMapClick = function (event) {
         if (search.map) {
           search.map.hideInfoWindow('searchMapWindow');
           search.selectedBike = undefined;
         }
       };
 
-      search.onBikeHover = function (bike) {
+      // show bike card in maps on card hover
+      search.onBikeHover = function (bike, toShow) {
         if (search.map) {
           search.selectedBike = bike;
-          search.map.showInfoWindow('searchMapWindow', search.map.markers[bike.id]);
+          if (toShow === true) {
+            search.map.showInfoWindow('searchMapWindow', search.map.markers[bike.id]);
+          } else if (toShow === false) {
+            search.map.hideInfoWindow('searchMapWindow');
+          }
         }
       };
 
-      search.onBikeHoverLeave = function () {
-        if (search.map) {
-          search.map.hideInfoWindow('searchMapWindow');
-        }
-      };
-
-      function populateBikes(location) {
-        search.bikes = undefined;
-
-        api.get("/rides?location=" + location).then(function(response) {
-          search.bikes = response.data;
-
-          if (search.bikes.length > 0) {
-            search.mapOptions.lat = search.bikes[0].lat_rnd;
-            search.mapOptions.lng = search.bikes[0].lng_rnd;
-            search.mapOptions.zoom = 10;
-          } else {
-            search.mapOptions.lat = 51.1657;
-            search.mapOptions.lng = 10.4515;
-            search.mapOptions.zoom = 4;
-          }
-        }, function(error) {
-        });
-      }
-
-      function setMetaTags(location) {
-        var data = {
-          location: location
-        };
-        console.log($translate.instant("search.meta-title", data));
-        $translate("search.meta-title", data).then(
-          function (translation) {
-            ngMeta.setTitle(translation);
-          }
-        );
-        ngMeta.setTitle($translate.instant("search.meta-title", data));
-        ngMeta.setTag("description", $translate.instant("search.meta-description", data));
-      }
-
+      search.initializeGoogleMap();
     }
   ]
 });
