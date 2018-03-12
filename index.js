5367aaed8455f332777d3625ae24d65e17dc6fcd
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-03-12 15:32:49 +0100

    disable forced https redirect for localhost and heroku review apps [WIP]

--- index.js
+++ index.js
@@ -5,36 +5,67 @@ var express = require('express');
 var expressEnforcesSSL = require('express-enforces-ssl');
 var prerender = require('prerender-node');
 var app = express();
-var logger = function (req, res, next) {
-  next();
+
+// force https redirect for staging and production
+// not used for local host and heroku review apps
+var httpForceRoute = function () {
+  // prerender
+  app.use(require('prerender-node').set('prerenderToken', 'W8S4Xn73eAaf8GssvVEw'));
+
+  // setting proper http headers
+  app.use(helmet());
+
+  // redirect to https
+  app.enable('trust proxy');
+  app.use(expressEnforcesSSL());
 };
 
-// prerender
-app.use(require('prerender-node').set('prerenderToken', 'W8S4Xn73eAaf8GssvVEw'));
+// refirect to proper domain on staging and production
+// not used for local host and heroku review apps
+var redirectToProperDomain = function (req, res, next) {
+  var correctHostname = stripTrailingSlash(determineHostname(req.subdomains, req.hostname));
+  var correctOriginalUrl = stripTrailingSlash(req.originalUrl);
+  if (req.hostname === correctHostname && req.originalUrl === correctOriginalUrl) {
+    next();
+  } else {
+    next();
+    res.redirect(301, "https://" + correctHostname + correctOriginalUrl);
+  }
+};
 
-// setting proper http headers
-app.use(helmet());
-
-// redirect to https
-app.enable('trust proxy');
-app.use(expressEnforcesSSL());
+// log the request
+// no functional use, only for debugging
+var logger = function (req) {
+  var origin = req.headers.host;
+  console.log("req obj: ", req);
+  console.log("origin: ", origin);
+  console.log("params: ", req.query);
+  console.log("is_shop params: ", req.query.is_shop);
+};
 
 // get port from env
 app.set('port', (process.env.PORT || 9003));
 
-// see all transactions through server
-app.use(logger);
-
-var determineHostname = function(subdomains, hostname) {
+var determineHostname = function (subdomains, hostname) {
   var domainPrefix = "www.";
   var domainEnding = retrieveTld(hostname);
   for (var i = 0; i < subdomains.length; i++) {
     switch (subdomains[i]) {
-      case "en": domainEnding = ".com"; break;
-      case "de": domainEnding = ".de"; break;
-      case "nl": domainEnding = ".nl"; break;
-      case "it": domainEnding = ".it"; break;
-      case "es": domainEnding = ".es"; break;
+      case "en":
+        domainEnding = ".com";
+        break;
+      case "de":
+        domainEnding = ".de";
+        break;
+      case "nl":
+        domainEnding = ".nl";
+        break;
+      case "it":
+        domainEnding = ".it";
+        break;
+      case "es":
+        domainEnding = ".es";
+        break;
     }
     if (subdomains[i] === "staging") {
       domainPrefix = "www.staging.";
@@ -43,29 +74,26 @@ var determineHostname = function(subdomains, hostname) {
   return domainPrefix + "listnride" + domainEnding;
 };
 
-var stripTrailingSlash = function(url) {
+var stripTrailingSlash = function (url) {
   // return url.replace(/\/+$/, "");
   return url;
 };
 
-var retrieveTld = function(hostname) {
+var retrieveTld = function (hostname) {
   return hostname.replace(/^(.*?)\listnride/, "");
 };
 
 // proper redirects
-app.use(function(req, res, next) {
-  var correctHostname = stripTrailingSlash(determineHostname(req.subdomains, req.hostname));
-  var correctOriginalUrl = stripTrailingSlash(req.originalUrl);
-  if (req.hostname === correctHostname && req.originalUrl === correctOriginalUrl) {
-    next();
-  } else {
-    res.redirect(301, "https://" + correctHostname + correctOriginalUrl);
-  }
+app.use(function (req, res, next) {
+  // redirectToProperDomain(req, res, next);
+  next();
 });
 
 // by default serves index.html
 // http://expressjs.com/en/4x/api.html#express.static
-app.use(express.static(__dirname.concat('/listnride/dist'), {index: 'index.html'}));
+app.use(express.static(__dirname.concat('/listnride/dist'), {
+  index: 'index.html'
+}));
 
 /*
 removing this will disable serving urls from browser
@@ -78,12 +106,7 @@ that is because 'angular-sanitize.min.js.map' is missing
 and chrome requests it. not for safari and firefox
 */
 app.use('/*', function (req, res) {
-  var origin = req.headers.host;
-  console.log("origin: ", origin);
-  console.log("params: ", req.query);
-  console.log("is_shop params: ", req.query.is_shop);
   res.sendFile(__dirname.concat('/listnride/dist/index.html'));
 });
 
-app.listen(app.get('port'), function () {
-});
+app.listen(app.get('port'));
