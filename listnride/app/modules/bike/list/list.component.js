d07aa7aafaf6acdfd1810d24f32b78f96daf91bf
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-05-19 19:38:58 +0300

    add insurance tab

--- listnride/app/modules/bike/list/list.component.js
+++ listnride/app/modules/bike/list/list.component.js
@@ -36,7 +36,7 @@ angular.module('list', ['ngLocale'])
 
         var list = this;
 
-        list.form = {images: []};
+        list.form = {images: [], bikeValue: 1000};
         list.selectedIndex = 0;
         list.removedImages = [];
         list.startImage = 1;
@@ -49,6 +49,9 @@ angular.module('list', ['ngLocale'])
         bikeOptions.allCategoriesOptions().then(function (resolve) {
           list.categoryOptions = resolve;
         });
+        list.insuranceFeeDefault = 1;
+        list.currencySign = '€';
+        list.bikeValueOptions = [1000,2000,3000,4000,5000];
 
         var setBusinessForm = function() {
           if (authentication.isBusiness) {
