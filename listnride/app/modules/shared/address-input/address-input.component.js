161c0e923c87b267c620e567f53e1a462bb51cf5
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-06-03 16:16:00 +0200

    update event tracking

--- listnride/app/modules/shared/address-input/address-input.component.js
+++ listnride/app/modules/shared/address-input/address-input.component.js
@@ -42,6 +42,8 @@ angular.module('addressInput',[]).component('addressInput', {
           addressInput.addressForm.$setTouched();
           addressInput.addressForm.$validate();
 
+          addressInput.addressFor.triggerHandler('submit');
+
           // addressInput.addressForm.$setSubmitted();
         }
       }
