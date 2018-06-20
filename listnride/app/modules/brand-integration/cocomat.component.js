12d3260f52598d39eb9989abd1dbf02480455b60
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-06-20 15:13:49 +0200

    use shops instead of locations as selector

--- listnride/app/modules/brand-integration/cocomat.component.js
+++ listnride/app/modules/brand-integration/cocomat.component.js
@@ -9,28 +9,50 @@ angular.module('cocomatIntegration',[]).component('cocomat', {
       $tpl.addPart(ENV.staticTranslation);
 
       cocomat.currentBikes = [];
-      cocomat.bikes = {
-        athina: [],
-        alimos: [],
-        kifisia: [],
-        pireas: []
+      cocomat.shops = {
+        6059: {
+          name: "Coco-Mat Hotel Kolonaki",
+          bikes: []
+        },
+        6061: {
+          name: "Hotel Coco-mat Nafsika",
+          bikes: []
+        },
+        6063: {
+          name: "Coco-mat Alimo",
+          bikes: []
+        },
+        6064: {
+          name: "Coco-mat Store Kalamaki",
+          bikes: []
+        },
+        6065: {
+          name: "Coco-mat Store Voula",
+          bikes: []
+        },
+        6066: {
+          name: "Coco-mat Store - Zea's Marina",
+          bikes: []
+        }
       };
 
-      api.get('/rides?family=31').then(
+      api.get('/rides?family=32').then(
         function (success) {
           for (var i=0; i<success.data.bikes.length; i++) {
-            switch (success.data.bikes[i].city) {
-              case "Athina": cocomat.bikes.athina.push(success.data.bikes[i]); break;
-              case "Alimos": cocomat.bikes.alimos.push(success.data.bikes[i]); break;
-              case "Kifisia": cocomat.bikes.kifisia.push(success.data.bikes[i]); break;
-              case "Kifisia": cocomat.bikes.kifisia.push(success.data.bikes[i]); break;
-            }
+            cocomat.shops[success.data.bikes[i].user_id].bikes.push(success.data.bikes[i]);
           }
         },
         function (error) {
           console.log('Error fetching Bikes');
         }
       );
+
+      cocomat.showBikesIn = function(shopId) {
+        console.log(shopId);
+        // cocomat.currentCity = cocomat.shops[shopId].name;
+        // cocomat.currentBikes = cocomat.shops[shopId].bikes;
+      }
     }
+
   ]
 });
