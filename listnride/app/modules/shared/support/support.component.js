47c7d8b7a680af5d978759e63c6e9ee46a32c177
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-09-19 18:27:31 +0300

    add support footer to a separate component -i

---
+++ listnride/app/modules/shared/support/support.component.js
@@ -0,0 +1,10 @@
+'use strict';
+angular.module('support', []).component('support', {
+  templateUrl: 'app/modules/shared/support/support.template.html',
+  controllerAs: 'support',
+  controller: [
+    function SupportController() {
+      var support = this;
+    }
+  ]
+});
