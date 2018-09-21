e0b36d26a3418c64418f9911efd365963c00f4bf
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-09-21 18:05:55 +0300

    add map view, add clusters refresh on Brands Page -i

--- listnride/app/modules/brands/brands.component.js
+++ listnride/app/modules/brands/brands.component.js
@@ -3,8 +3,8 @@
 angular.module('brands', []).component('brands', {
   templateUrl: 'app/modules/brands/brands.template.html',
   controllerAs: 'brands',
-  controller: ['$translatePartialLoader', 'ENV', 'notification',
-    function BrandsController($tpl, ENV, notification) {
+  controller: ['$timeout', '$translatePartialLoader', 'ENV', 'notification', 'mapConfigs', 'NgMap',
+    function BrandsController($timeout, $tpl, ENV, notification, mapConfigs, NgMap) {
 
       var brands = this;
       $tpl.addPart(ENV.staticTranslation);
@@ -14,9 +14,19 @@ angular.module('brands', []).component('brands', {
         brands.isMapView = false;
         brands.categoryIds = [];
         brands.selectedCategories = [];
+        brands.filteredBrands = [];
         brands.data = [];
+        brands.colorScheme = mapConfigs.colorScheme();
+        // TODO: change to first picked Brand
+        brands.mapOptions = {
+          lat: 50.1176084,
+          lng: 11.362239,
+          zoom: 5
+        };
         // methods
         brands.isIncludeCategory = isIncludeCategory;
+        brands.filterChange = filterChange;
+        brands.checkSelectedBrands = checkSelectedBrands;
         // invocations
         getData();
       };
@@ -83,8 +93,8 @@ angular.module('brands', []).component('brands', {
                   "lng": 13.403609690991
                 },
                 {
-                  "lat": 52.5269957477477,
-                  "lng": 13.403609690991
+                  "lat": 42.5269957477477,
+                  "lng": 23.403609690991
                 }
               ]
             },
@@ -95,12 +105,12 @@ angular.module('brands', []).component('brands', {
               "tile_image": "/app/assets/ui_images/featured-tiles/rethink.jpg",
               "categories": [21, 22, 31],
               "pins": [{
-                  "lat": 52.5269957477477,
-                  "lng": 13.403609690991
+                  "lat": 12.5269957477477,
+                  "lng": 10.403609690991
                 },
                 {
-                  "lat": 52.5269957477477,
-                  "lng": 13.403609690991
+                  "lat": 12.5269957477477,
+                  "lng": 15.403609690991
                 }
               ]
             },
@@ -111,12 +121,12 @@ angular.module('brands', []).component('brands', {
               "tile_image": "/app/assets/ui_images/featured-tiles/vello.jpg",
               "categories": [41, 51, 61],
               "pins": [{
-                  "lat": 52.5269957477477,
-                  "lng": 13.403609690991
+                  "lat": 11.5269957477477,
+                  "lng": 5.403609690991
                 },
                 {
-                  "lat": 52.5269957477477,
-                  "lng": 13.403609690991
+                  "lat": 25.5269957477477,
+                  "lng": 80.403609690991
                 }
               ]
             },
@@ -201,6 +211,8 @@ angular.module('brands', []).component('brands', {
               ]
             }
           ]
+          checkSelectedBrands();
+          initializeGoogleMap();
       };
 
       function isIncludeCategory(brandCategories) {
@@ -208,6 +220,114 @@ angular.module('brands', []).component('brands', {
         if (!brands.categoryIds.length) return true;
         return !!_.intersection(brands.categoryIds, brandCategories).length;
       };
+
+      function filterChange() {
+        brands.checkSelectedBrands();
+        $timeout(function () {
+          refreshMarkerCluster(brands.map);
+        }, 0);
+      };
+
+      function checkSelectedBrands() {
+        brands.filteredBrands = [];
+        // if no categories were pick, we show all brands
+        if (brands.categoryIds.length) {
+          _.forEach(brands.data, function (brand) {
+            if (_.intersection(brands.categoryIds, brand.categories).length) brands.filteredBrands.push(brand);
+          });
+        } else {
+          brands.filteredBrands = brands.data;
+        }
+      }
+
+      // ============================
+      // >>>> START MAP FUNCTIONALITY
+      // ============================
+
+
+      function initializeGoogleMap() {
+        // without timeout map will take an old array with bikes
+        $timeout(function(){
+          NgMap.getMap({ id: "searchMap" }).then(function (map) {
+            // map.fitBounds(correctBounds());
+            map.setZoom(map.getZoom());
+            initMarkerClusterer(map);
+            brands.map = map;
+          });
+        }, 0);
+      }
+
+      // Clear Markers, Add new and redraw map on each state update
+      function refreshMarkerCluster(map) {
+        var markers = [];
+        _.forEach(brands.filteredBrands, function (brand) {
+          _.forEach(brand.pins, function (pin) {
+            markers.push(createMarkerForBike(pin, map, brand));
+          })
+        });
+
+        brands.clusterer.clearMarkers();
+        /**
+         * Add new markers and redraw a map
+         * @param {Array} markers google.maps.Marker
+         * @param {Boolean} opt_nodraw
+         */
+        brands.clusterer.addMarkers(markers, false);
+      }
+
+      function initMarkerClusterer(map) {
+        var markers = [];
+
+        _.forEach(brands.filteredBrands, function(brand){
+          _.forEach(brand.pins, function(pin){
+            markers.push(createMarkerForBike(pin, map, brand));
+          })
+        });
+
+        brands.mapMarkers = markers;
+
+        var mcOptions = {
+          styles: mapConfigs.clustersStyle()
+        };
+        brands.clusterer = new MarkerClusterer(map, markers, mcOptions);
+        return brands.clusterer
+      }
+
+      function createMarkerForBike(pin, map, brand) {
+        // Origins, anchor positions and coordinates of the marker increase in the X
+        // direction to the right and in the Y direction down.
+        var image = {
+          url: 'app/assets/ui_icons/map/markers/Pin Map 56x56.png',
+          // This marker is 56 pixels wide by 56 pixels high.
+          size: new google.maps.Size(56,56),
+          // The origin for this image is (0, 0).
+          origin: new google.maps.Point(0, 0),
+          // The anchor for this image is the base of the flagpole at (56/2, 56).
+          anchor: new google.maps.Point(28, 56),
+          // The label position inside marker
+          labelOrigin: new google.maps.Point(28, 22)
+          // scaledSize: new google.maps.Size(50, 50)
+        };
+
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(pin.lat, pin.lng),
+          id: brand.id,
+          icon: image,
+          title: brand.title,
+          label: { text: brand.title, color: "white", fontSize: '13px', fontWeight: 'bold' }
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+          brands.selectedBrand = brand;
+          map.showInfoWindow('searchMapWindow', this);
+        });
+
+        return marker;
+      }
+
+      // ============================
+      // END MAP FUNCTIONALITY <<<<<<
+      // ============================
     }
   ]
 });
