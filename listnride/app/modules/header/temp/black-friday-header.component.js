4dbf0f848f8a72fe3ca68173ce4e5c98a4830ccf
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-11-20 18:10:59 +0200

    added component for Black Friday top banner

---
+++ listnride/app/modules/header/temp/black-friday-header.component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('blackFridayHeader', []).component('blackFridayHeader', {
+  templateUrl: 'app/modules/header/temp/black-friday-header.template.html',
+  controllerAs: 'blackFridayHeader',
+  controller: [function BlackFridayHeader() {
+      var blackFridayHeader = this;
+
+    }
+  ]
+});
