5fea1b7890755f940199a1db462bab4d5d41dc98
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-02-08 19:04:25 +0200

    refactored code in event page

--- listnride/app/modules/events/event.component.js
+++ listnride/app/modules/events/event.component.js
@@ -9,31 +9,21 @@ angular.module('event', []).component('event', {
       $tpl.addPart('static');
 
       event.$onInit = function() {
-
-        //VARIABLES
-        event.sizes = [];
-        event.bikes = [];
-        event.hasLogo = false;
-
-        //METHODS
-        event.getRequestUrl = getRequestUrl;
-        event.getEvents = getEvents;
-
-        // INVOCATIONS
+        event.name = $stateParams.event_name;
         event.object = {
-          'berlin-triathlon'       : event.getRequestUrl('30', 'Berlin', '', '2019-06-02'),
-          'berlin-triathlon-xl'    : event.getRequestUrl('30,31', 'Berlin', '', '2019-06-23'),
-          'cyclassics-hamburg'     : event.getRequestUrl('30', 'Hamburg','cyclassics-hamburg', '2018-08-19'),
-          'triathlon-hamburg'      : event.getRequestUrl('30', 'Hamburg','', '2018-07-14'),
-          'toros-de-gravel'        : event.getRequestUrl('42,43', 'Mallorca','', '2018-10-13'),
-          'riderman-rothaus'       : event.getRequestUrl('20', 'Bad Dürrheim','', ''),
-          'velothon-bikerental'    : event.getRequestUrl('30', 'Berlin', 'velothon', '2018-05-13'),
-          'vatternrundan'          : event.getRequestUrl('30,31', 'Motala,Sweden', '', '2019-06-07'),
-          'epicgrancanaria'        : event.getRequestUrl('30,31,32,33', 'Gran Canaria, Provinz Las Palmas, Spanien', '', '2019-04-05'),
-          'velorace-dresden'       : event.getRequestUrl('30', 'Dresden', '', '2019-08-11'),
-          'lardita-arezzo'         : event.getRequestUrl('30', 'Arezzo, Tuscany', '', '2019-03-24'),
-          'granfondo-via-del-sale' : event.getRequestUrl('30', 'Cesenatico', '', '2019-05-05'),
-          'giro-sardegna'          : event.getRequestUrl('30', 'Cagliari', '', '2019-04-21')
+          'berlin-triathlon': getRequestUrl('30', 'Berlin', '', '2019-06-02'),
+          'berlin-triathlon-xl': getRequestUrl('30,31', 'Berlin', '', '2019-06-23'),
+          'cyclassics-hamburg': getRequestUrl('30', 'Hamburg', 'cyclassics-hamburg', '2018-08-19'),
+          'triathlon-hamburg': getRequestUrl('30', 'Hamburg', '', '2018-07-14'),
+          'toros-de-gravel': getRequestUrl('42,43', 'Mallorca', '', '2018-10-13'),
+          'riderman-rothaus': getRequestUrl('20', 'Bad Dürrheim', '', ''),
+          'velothon-bikerental': getRequestUrl('30', 'Berlin', 'velothon', '2018-05-13'),
+          'vatternrundan': getRequestUrl('30,31', 'Motala,Sweden', '', '2019-06-07'),
+          'epicgrancanaria': getRequestUrl('30,31,32,33', 'Gran Canaria, Provinz Las Palmas, Spanien', '', '2019-04-05'),
+          'velorace-dresden': getRequestUrl('30', 'Dresden', '', '2019-08-11'),
+          'lardita-arezzo': getRequestUrl('30', 'Arezzo, Tuscany', '', '2019-03-24'),
+          'granfondo-via-del-sale': getRequestUrl('30', 'Cesenatico', '', '2019-05-05'),
+          'giro-sardegna': getRequestUrl('30', 'Cagliari', '', '2019-04-21')
 
           // CUSTOM DESIGN AND OLD PAGES
 
@@ -52,24 +42,28 @@ angular.module('event', []).component('event', {
           // 'pushnpost'              : '/users/1998',
           // 'kuchenundraketen'       : '/users/1998'
         };
+        if (!event.object[event.name]) $state.go('404');
 
+        // VARIABLES
+        event.sizes = [];
+        event.bikes = [];
+        event.hasLogo = false;
+        event.path = event.object[event.name];
+        event.imagePath = 'app/assets/ui_images/events/' + event.name + '_hero.jpg';
         bikeOptions.sizeOptions('search', null).then(function (resolve) {
           event.sizes = resolve;
         });
 
-        event.name = $stateParams.event_name;
-
-        event.path = event.object[event.name];
-
-        event.imagePath = 'app/assets/ui_images/events/' + event.name + '_hero.jpg';
-
-        if (!event.path) $state.go('404');
-
-        if(event.name === 'vatternrundan') {
+        // TODO: maybe we should delete this, because it's only one event with logo
+        if (event.name === 'vatternrundan') {
           event.hasLogo = true;
           event.logoPath = 'app/assets/ui_images/events/' + event.name + '_logo.png';
         }
 
+        // METHODS
+        event.getEvents = getEvents;
+
+        // INVOCATIONS
         event.getEvents();
       }
 
@@ -91,12 +85,12 @@ angular.module('event', []).component('event', {
 
       function getEvents() {
         api.get(event.path).then(
-            function (response) {
-              event.bikes = response.data.bikes;
-            },
-            function (error) {
-              notification.show(error);
-            }
+          function (response) {
+            event.bikes = response.data.bikes;
+          },
+          function (error) {
+            notification.show(error);
+          }
         );
       }
     }
