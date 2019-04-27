0187c93d2a6e7de8484217fb2e090479023635fa
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-04-27 19:27:59 +0300

    Add scheduler presets and basic navigation

--- listnride/app/modules/booking-calendar/booking-calendar.component.js
+++ listnride/app/modules/booking-calendar/booking-calendar.component.js
@@ -1,6 +1,13 @@
-import { Scheduler } from '../../../js_modules/bryntum-scheduler/scheduler.module.min';
+import {
+  Scheduler,
+  PresetManager,
+  DateHelper
+} from '../../../js_modules/bryntum-scheduler/scheduler.module.min';
 
 import './booking-calendar.css';
+
+const dropTimezone = date =>
+  new Date(`${DateHelper.format(date, 'YYYY-MM-DD')}T00:00:00Z`);
 
 angular.module('bookingCalendar', []).component('bookingCalendar', {
   templateUrl: 'app/modules/booking-calendar/booking-calendar.template.html',
@@ -14,34 +21,152 @@ angular.module('bookingCalendar', []).component('bookingCalendar', {
     };
 
     bookingCalendar.gotoToday = () => {
-      console.log('go to today');
+      bookingCalendar.scheduler.setTimeSpan(
+        ...viewPresetOptions
+          .get(bookingCalendar.scheduler.viewPreset.name)
+          .getTimeSpan(new Date())
+      );
+    };
+
+    const viewPresetOptions = new Map([
+      [
+        'week',
+        {
+          key: 'week',
+          label: 'booking-calendar.week',
+          getTimeSpan: date => {
+            const firstday = DateHelper.add(
+              date,
+              -1 * date.getDay() + 1,
+              'day'
+            );
+            const lastday = DateHelper.add(firstday, 6, 'day');
+            return [firstday, lastday].map(dropTimezone);
+          }
+        }
+      ],
+      [
+        'month',
+        {
+          key: 'month',
+          label: 'booking-calendar.month',
+          getTimeSpan: date => {
+            console.log(date);
+            return [
+              DateHelper.getFirstDateOfMonth(date),
+              DateHelper.getLastDateOfMonth(date)
+            ].map(dropTimezone);
+          }
+        }
+      ]
+    ]);
+
+    bookingCalendar.viewPresetOptions = Array.from(viewPresetOptions.values());
+
+    bookingCalendar.setViewPreset = presetName => {
+      const anchorDate = bookingCalendar.scheduler.startDate; // date that new time span should contain
+      bookingCalendar.scheduler.setTimeSpan(
+        ...viewPresetOptions.get(presetName).getTimeSpan(anchorDate)
+      );
+      bookingCalendar.scheduler.setViewPreset(presetName);
+    };
+
+    bookingCalendar.shiftPrevious = () => {
+      bookingCalendar.scheduler.shiftPrevious();
+    };
+
+    bookingCalendar.shiftNext = () => {
+      bookingCalendar.scheduler.shiftNext();
     };
 
     function initScheduler() {
-      window.scheduler = new Scheduler({
-        appendTo: document.querySelector('.scheduler-container'),
+      PresetManager.registerPreset('week', {
+        tickWidth: 150,
+        displayDateFormat: 'MMMM DD, HH:mm',
+        shiftUnit: 'week',
+        shiftIncrement: 1,
+        timeResolution: {
+          unit: 'week',
+          increment: 1
+        },
+        headerConfig: {
+          top: {
+            unit: 'month',
+            dateFormat: 'MMMM YYYY'
+          },
+          middle: {
+            unit: 'day',
+            dateFormat: 'dddd DD'
+          }
+        }
+      });
 
-        columns: [{
-          text: 'Name',
-          field: 'name',
-          width: 300,
-        }],
-        
+      PresetManager.registerPreset('month', {
+        tickWidth: 50,
+        displayDateFormat: 'MMMM DD, HH:mm',
+        shiftUnit: 'month',
+        shiftIncrement: 1,
+        timeResolution: {
+          unit: 'month',
+          increment: 1
+        },
+        headerConfig: {
+          top: {
+            unit: 'month',
+            dateFormat: 'MMMM YYYY'
+          },
+          middle: {
+            unit: 'day',
+            dateFormat: 'DD'
+          }
+        }
+      });
+
+      const defaultPreset = 'month';
+      const [startDate, endDate] = viewPresetOptions
+        .get(defaultPreset)
+        .getTimeSpan(new Date());
+
+      bookingCalendar.scheduler = new Scheduler({
+        appendTo: document.querySelector('.scheduler-container'),
         readOnly: true,
 
-        features : {
+        columns: [
+          {
+            type: 'tree',
+            text: 'Name',
+            field: 'name',
+            width: 300
+          }
+        ],
+        rowHeight: 85,
+
+        zoomOnTimeAxisDoubleClick: false,
+        viewPreset: defaultPreset,
+        weekStartDay: 1, // monday
+
+        features: {
           contextMenu: false,
           eventFilter: false,
           headerContextMenu: false,
           timeRanges: {
             showCurrentTimeLine: true,
-            showHeaderElements  : true,
+            showHeaderElements: true
           },
+          tree: true
         },
 
-        resources: [{
+        resources: [
+          {
             id: 1,
-            name: 'Dan Stevenson'
+            name: 'Dan Stevenson',
+            expanded: true,
+            children: [
+              {
+                id: 3,
+                name: 'Barbra Streisand'
+              }
+            ]
           },
           {
             id: 2,
@@ -53,22 +178,24 @@ angular.module('bookingCalendar', []).component('bookingCalendar', {
           // the date format used is configurable, defaults to the simplified ISO format (e.g. 2017-10-05T14:48:00.000Z)
           // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
           {
-            resourceId: 1,
-            startDate: '2019-04-11',
-            endDate: '2019-04-15',
-            name: 'foo',
+            resourceId: 3,
+            startDate: '2019-04-11 08:00:00',
+            endDate: '2019-04-15 12:00:00',
+            name: 'foo'
           },
           {
             resourceId: 2,
-            startDate: '2019-04-13',
-            endDate: '2019-04-19',
-            name: 'bar',
+            startDate: '2019-04-13 15:00:00',
+            endDate: '2019-04-19 12:30:00',
+            name: 'bar'
           }
         ],
 
-        startDate: new Date(2019, 3, 1),
-        endDate: new Date(2019, 3, 30)
+        startDate,
+        endDate
       });
+
+      window.scheduler = bookingCalendar.scheduler;
     }
   }
 });
