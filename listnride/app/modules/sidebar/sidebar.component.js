583650bfc34ed8cc2c0e01164f0e24638f463f42
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-06-27 19:37:25 +0300

    fixed booking calendar hiding for non-business user in mobile menu, fixed create/hide booking calendar link for new users (after sign up)

--- listnride/app/modules/sidebar/sidebar.component.js
+++ listnride/app/modules/sidebar/sidebar.component.js
@@ -1,18 +1,14 @@
-(function(){
-'use strict';
 angular.module('header').component('sidebar', {
   templateUrl: 'app/modules/sidebar/sidebar.template.html',
   controllerAs: 'sidebar',
-  controller: ['$mdSidenav', '$localStorage', 'authentication',
-    function SidebarController($mdSidenav, $localStorage, authentication) {
-      var sidebar = this;
-      sidebar.authentication = authentication;
-      sidebar.userId = $localStorage.userId;
-      
-      sidebar.toggle = function() {
-        $mdSidenav('right').toggle();
-      };
-    }
-  ]
+  controller: function SidebarController($scope, $mdSidenav, $localStorage, authentication) {
+    let sidebar = this;
+    sidebar.authentication = authentication;
+    sidebar.userId = $localStorage.userId;
+    sidebar.isUserBusiness = sidebar.authentication.isBusiness;
+
+    sidebar.toggle = function() {
+      $mdSidenav('right').toggle();
+    };
+  }
 });
-})();
