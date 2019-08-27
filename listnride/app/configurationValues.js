f60264b40dde62abd7d126c62e99e340bae28125
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-08-27 11:44:42 +0300

    added keys for recaptcha

--- listnride/app/configurationValues.js
+++ listnride/app/configurationValues.js
@@ -1,15 +1,11 @@
 export const API_ENDPOINT = process.env.LNR_API_ENDPOINT;
-
 export const USER_ENDPOINT = process.env.LNR_USER_ENDPOINT;
-
 export const WEBAPP_URL = process.env.LNR_WEBAPP_URL;
-
 export const HTML_5_MODE = process.env.LNR_HTML_5_MODE === 'true';
-
 export const API_KEY_BRAINTREE = process.env.LNR_API_KEY_BRAINTREE;
-
 export const API_BRAINTREE_ENV = process.env.LNR_API_BRAINTREE_ENV;
-
 export const API_KEY_FACEBOOK_PLATFORM = process.env.LNR_API_KEY_FACEBOOK_PLATFORM;
-
 export const API_KEY_GOOGLE_MAPS = process.env.LNR_API_KEY_GOOGLE_MAPS;
+export const API_KEY_RECAPTCHA_PUBLIC = process.env.LNR_API_RECAPTCHA_V3_PUBLIC;
+// export const ADYEN_ENV = process.env.LNR_ADYEN_ENV;
+// export const ADYEN_ORIGIN_KEY = process.env.LNR_ADYEN_ORIGIN_KEY;
