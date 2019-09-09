ef8795674ec813510a201ef9a4c398bd19ff997c
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-09-09 12:15:11 +0300

    added application helper, removed helper

---
+++ listnride/app/services/application-helper/application-helper.service.js
@@ -0,0 +1,11 @@
+angular
+  .module('listnride')
+  .factory('applicationHelper', function(){
+
+
+
+    return {
+      emailPattern: /(?!^[.+&'_-]*@.*$)(^[_\w\d+&'-]+(\.[_\w\d+&'-]*)*@[\w\d-]+(\.[\w\d-]+)*\.(([\d]{1,3})|([\w]{2,}))$)/i,
+      phonePattern: /^\+(?:[0-9] ?){6,14}[0-9]$/
+    }
+  });
